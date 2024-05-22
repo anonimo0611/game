c0a201d0dd33f700c56f7301b409aedd6568e42c
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2024-05-22 18:36:37 +0900

    Add files via upload

--- _study/DragonWarrior-Dragonlord/lib/state.js
+++ _study/DragonWarrior-Dragonlord/lib/state.js
@@ -1,7 +1,5 @@
 export class State {
 	#state = '';
-	#defaultState;
-	#lastState;
 	init() {
 		this.Enum = Object.create(null)
 		for (const [key, val] of entries(this)) {
@@ -9,27 +7,15 @@ export class State {
 			if (state) this.Enum[state] = state; else continue
 			if (this.#state === '' && val === true) this.#state = state
 			defineProperty(this, key, {get(){return this.#state === state}})
+			this['switchTo'+state] = data=> this.switchTo(state, data);
 		}
 		freeze(this.Enum)
-		this.#lastState    = 
-		this.#defaultState = this.current
 	}
-	get current()  {
-		return String(this.#state)
-	}
-	get last()  {
-		return this.#lastState;
-	}
-	some(stateStr){
-		return String(stateStr).split('|').some(k=> k === this.#state)
-	}
-	change(state) {
-		if (!this.Enum[state])
+	get current() {return String(this.#state)}
+	some(keyStr)  {return splitByBar(keyStr).some(k=> k === this.#state)}
+	switchTo(state) {
+		if (!hasOwn(this.Enum, state))
 			throw ReferenceError(`State '${state}' is not defined`)
-		this.#lastState = this.current;
-		return (this.#state = state);
-	}
-	reset() {
-		this.change(this.#defaultState)
+		this.#state = state;
 	}
 }
