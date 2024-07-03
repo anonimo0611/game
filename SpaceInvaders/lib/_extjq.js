a0ec3a38e8889e7af4fdbe2335abfb95b947efff
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2024-07-04 06:41:28 +0900

    Add files via upload

--- SpaceInvaders/lib/_extjq.js
+++ SpaceInvaders/lib/_extjq.js
@@ -6,6 +6,7 @@ const $off     = (...a)=> $(window).off    (...a);
 const $offon   = (...a)=> $(window).offon  (...a);
 const $one     = (...a)=> $(window).one    (...a);
 const $trigger = (...a)=> $(window).trigger(...a);
+const $load    = (func)=> $(window).on('load',func);
 const $ready   = (func)=> $(window).ready(func);
 Object.assign(HTMLElement.prototype, {
 	css    (...a) {return this.style.setProperty(...a),this},
