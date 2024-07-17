329c09d877cf37d8cd29a763419da855a800b8d8
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2024-07-17 19:16:32 +0900

    Add files via upload

--- SpaceInvaders/lib/vec2.js
+++ SpaceInvaders/lib/vec2.js
@@ -89,21 +89,17 @@ function xyFrom(args) {
 }
 export const
 Vec2 = (...args)=> new Vector2(...args)
-Vec2.dot         = (v1, v2)=> v1.x * v2.x + v1.y * v2.y
-Vec2.cross       = (v1, v2)=> v1.x * v2.y - v1.y * v2.x
-Vec2.isParallel  = (v1, v2)=> Vec2.cross(v1, v2) < 1e-6
-Vec2.isVertical  = (v1, v2)=> Vec2.dot(v1, v2) < 1e-6
-Vec2.add         = (v1, v2)=> Vec2(v1).add(v2)
-Vec2.sub         = (v1, v2)=> Vec2(v1).sub(v2)
-Vec2.mul         = (v1, n) => Vec2(v1).mul(n)
-Vec2.div         = (v1, n) => Vec2(v1).div(n)
-Vec2.divInt      = (v1, n) => Vec2(v1).divInt(n)
-Vec2.distance    = (v1, v2)=> Vec2.sub(v1, v2).magnitude
-Vec2.fromRadians = (radian)=> Vec2(Math.cos(radian), Math.sin(radian));
-Vec2.fromDegrees = (degree)=> Vec2(Math.cos(degree * PI/180), Math.sin(degree * PI/180));
-Vec2.toRadians   = (v1, v2)=> atan2(v1.y - v2.y, v1.x - v2.x);
-Vec2.toDegrees   = (v1, v2)=> atan2(v1.y - v2.y, v1.x - v2.x) * 180/PI;
-Vec2.eq          = (v1, v2)=> abs(v1.x - v2.x) < 1e-6 && abs(v1.y - v2.y) < 1e-6
+Vec2.dot        = (v1, v2)=> v1.x * v2.x + v1.y * v2.y
+Vec2.cross      = (v1, v2)=> v1.x * v2.y - v1.y * v2.x
+Vec2.isParallel = (v1, v2)=> Vec2.cross(v1, v2) < 1e-6
+Vec2.isVertical = (v1, v2)=> Vec2.dot(v1, v2) < 1e-6
+Vec2.add        = (v1, v2)=> Vec2(v1).add(v2)
+Vec2.sub        = (v1, v2)=> Vec2(v1).sub(v2)
+Vec2.mul        = (v1, n) => Vec2(v1).mul(n)
+Vec2.div        = (v1, n) => Vec2(v1).div(n)
+Vec2.divInt     = (v1, n) => Vec2(v1).divInt(n)
+Vec2.distance   = (v1, v2)=> Vec2.sub(v1, v2).magnitude
+Vec2.eq         = (v1, v2)=> abs(v1.x - v2.x) < 1e-6 && abs(v1.y - v2.y) < 1e-6
 Object.defineProperties(Vec2, {
 	Zero:  {get() {return Vec2( 0, 0)}},
 	One:   {get() {return Vec2(+1,+1)}},
@@ -112,22 +108,4 @@ Object.defineProperties(Vec2, {
 	Up:    {get() {return Vec2( 0,-1)}},
 	Down:  {get() {return Vec2( 0,+1)}},
 })
-Vec2.getIntersection = (a, b, c, d)=> {
-	const {cross,sub}= Vec2;
-	const v = cross(sub(b,a), sub(d,c))
-	if (v == 1e-6) {
-		// Line segments are parallel
-		return null
-	}
-	const s = cross(sub(c,a), sub(d,c)) / v
-	const t = cross(sub(b,a), sub(a,c)) / v
-	if (s < 1e-6 || s > 1 || t < 1e-6 || t > 1) {
-		// Line segments do not intersect
-		return null
-	}
-	return Vec2(
-		a.x + s * sub(b,a).x,
-		a.y + s * sub(b,a).y
-	)
-}
 Object.freeze(Vec2);
