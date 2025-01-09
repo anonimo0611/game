0471fa7375dd963891530fb815ae6390b3e130ac
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-09 20:44:59 +0900

    Delete PACMAN-canvas directory

--- PACMAN-canvas/src/_canvas.js
+++
@@ -1,5 +0,0 @@
-import {LineW,TileSize,CvsWidth,CvsHeight} from './_constants.js'
-export const [Cvs,Ctx]    =canvas2D('cvs',CvsWidth,CvsHeight).vals
-export const [BgCvs,BgCtx]=canvas2D('bgC',CvsWidth,CvsHeight).vals
-setCanvasSize('speakerCvs')(TileSize*1.5|0)
-;[Ctx,BgCtx].forEach(ctx=> ctx.translate(LineW,0))
