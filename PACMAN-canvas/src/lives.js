34d711c8120a6fd7e10091fdfaaec54fb467cf53
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-11 17:37:48 +0900

    Add files via upload

--- PACMAN-canvas/src/lives.js
+++ PACMAN-canvas/src/lives.js
@@ -8,9 +8,8 @@ import {TileSize as T} from './_constants.js'
 
 export const Lives = new class {
 	static {
-		const set = ()=> Lives.#set()
-		$(Form.lvsRng).on('input', set)
-		$on('Title Start Ready Restart', set)
+		$(Form.lvsRng).on('input', ()=> Lives.#set())
+		$on('Title Start Ready Restart', ()=> Lives.#set())
 	}
 	#left = 0
 	get left() {return Lives.#left}
@@ -18,21 +17,19 @@ export const Lives = new class {
 	#setCurrent() {
 		const {left,Max}=Lives
 		return this.#left = +{
-			'Title':  Max,
-			'Start':  Max+1,
-			'Ready':  State.lastIs('Start')? Max:left,
-			'Restart':left-1,
+			Title:  Max,
+			Start:  Max+1,
+			Ready:  State.lastIs('Start')? Max:left,
+			Restart:left-1,
 		}[State.current]
 	}
 	#draw(left=Lives.#setCurrent()) {
-		const Size   = T * 2
-		const Radius = T * 0.78
-		const sprite = new Sprite({Radius}, {closed:false})
+		const sprite = new Sprite({Radius:T*.78},{closed:false})
 		BgCtx.save()
 		BgCtx.translate(T*2, T*32)
-		BgCtx.clearRect(0,0, Size*5, Size)
+		BgCtx.clearRect(0,0, T*2*5, T*2)
 		for (let i=0; i<left; i++)
-			sprite.draw(BgCtx, Vec2(Size*i+T, T))
+			sprite.draw(BgCtx, Vec2(T*2*i,0).add(T))
 		BgCtx.restore()
 	}
 	#set()   {Lives.#draw()}
--- PACMAN-canvas/src/maze_wall.js
+++ PACMAN-canvas/src/maze_wall.js
@@ -2,26 +2,24 @@ import {BgCvs,BgCtx as ctx} from './_canvas.js'
 import {MapData,Color,TileSize as T,ColMax as W} from './_constants.js'
 
 export const MazeWall = new class {
-	#drawCorner(chip, x, y, type=0) {
-		const idx = isNum(chip)
-			? chip-1 : 'ABCD'.indexOf(chip.toUpperCase())
+	#drawCorner(cornerIdx, x, y, type=0) {
 		ctx.save()
 		ctx.translate(x+T/2, y+T/2)
-		ctx.scale(...[[1,1],[-1,1],[-1,-1],[1,-1]][idx])
+		ctx.scale(...[[1,1],[-1,1],[-1,-1],[1,-1]][cornerIdx])
 		ctx.beginPath()
-		type == 2
+		;(type == 2)
 			? cvsStrokeLine(ctx)(T/2,T/2, T/2-2,T/2-2)
-			: ctx.arc(T/2, T/2, (type? T-2:T/2), PI, PI*1.5)
+			: ctx.arc(T/2,T/2, (type? T-2:T/2), PI,PI*1.5)
 		ctx.stroke()
 		ctx.restore()
 	}
 	#drawGhostPen() {
-		const LH = ctx.lineWidth/2
+		const L = ctx.lineWidth/2
 		cvsSetNewLinePath(ctx)(
-		[13.00*T-LH, 13.55*T],[10.55*T, 13.55*T],[10.55*T,    17.45*T],
-		[17.45*T,    17.45*T],[17.45*T, 13.55*T],[15.00*T+LH, 13.55*T],
-		[15.00*T+LH, 13.90*T],[17.10*T, 13.90*T],[17.10*T,    17.10*T],
-		[10.90*T,    17.10*T],[10.90*T, 13.90*T],[13.00*T-LH, 13.90*T])
+		[13.00*T-L, 13.55*T],[10.55*T, 13.55*T],[10.55*T,   17.45*T],
+		[17.45*T,   17.45*T],[17.45*T, 13.55*T],[15.00*T+L, 13.55*T],
+		[15.00*T+L, 13.90*T],[17.10*T, 13.90*T],[17.10*T,   17.10*T],
+		[10.90*T,   17.10*T],[10.90*T, 13.90*T],[13.00*T-L, 13.90*T])
 		ctx.closePath()
 		ctx.stroke()
 	}
@@ -34,28 +32,28 @@ export const MazeWall = new class {
 		this.#drawGhostPen()
 		ctx.restore()
 	}
-	#drawTile = (chipStr, idx)=> {
-		const chip = toNumber(chipStr)
-		const [tx,ty]= [idx%W, idx/W|0]
+	#drawTile = (c, i)=> {
+		const [tx,ty]= [i%W, i/W|0]
 		const [px,py]= [tx*T, ty*T]
+		const ci = +c? c-1 : 'ABCD'.indexOf(c.toUpperCase())
 
-		;/[ABCD]/.test(chip) && this.#drawCorner(chip, px, py, 1)
-		;/[abcd]/.test(chip) && this.#drawCorner(chip, px, py, 2)
-		;/[ABCD1234]/i.test(chip) && this.#drawCorner(chip, px, py)
+		;/[ABCD]/.test(c) && this.#drawCorner(ci, px, py, 1)
+		;/[abcd]/.test(c) && this.#drawCorner(ci, px, py, 2)
+		;/[ABCD1234]/i.test(c) && this.#drawCorner(ci, px, py)
 
-		;(chip == '-')     && cvsStrokeLine(ctx)(px, py+T/2, px+T, py+T/2)
-		;/[#|]/.test(chip) && cvsStrokeLine(ctx)(px+T/2, py, px+T/2, py+T)
+		;(c == '-')     && cvsStrokeLine(ctx)(px, py+T/2, px+T, py+T/2)
+		;/[#|]/.test(c) && cvsStrokeLine(ctx)(px+T/2, py, px+T/2, py+T)
 
-		if (chip == '#' || (!tx || tx == W-1) && isNum(chip)) {
+		if (c == '#' || (!tx || tx == W-1) && +c) {
 			const oX = tx < W/2 ? -T/2+2 : T/2-2
 			cvsStrokeLine(ctx)(px+T/2+oX, py, px+T/2+oX, py+T)
 		}
-		if (/[=_]/.test(chip) || ty == 1 && isNum(chip)) {
-			const oY = /[=12]/.test(chip) ? -T/2+2 : T/2-2
+		if (/[=_]/.test(c) || ty == 1 && +c) {
+			const oY = /[=12]/.test(c) ? -T/2+2 : T/2-2
 			ctx.save()
 			ctx.translate(px, py+T/2)
 			cvsStrokeLine(ctx)(0, oY, T, oY)
-			!isNum(chip) && cvsStrokeLine(ctx)(0,0, T,0)
+			!+c && cvsStrokeLine(ctx)(0,0, T,0)
 			ctx.restore()
 		}
 	}
