12f18ab2c262fe7cad332a799118b6fdcda74364
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-12 22:35:16 +0900

    Add files via upload

--- PONG/lib/wheel.js
+++ PONG/lib/wheel.js
@@ -4,17 +4,14 @@ const dBody = document.body;
 		dBody.querySelectorAll('input[type=range]')
 			.forEach(ctrl=> this.setupRangeCtrl(ctrl));
 	}
-	vals = r=> [[r.value],[r.step,1],[r.min,0],[r.max,100]].map(a=> toNumber(...a));
 	setupRangeCtrl(ctrl) { // Labels must be block-level
 		const label = ctrl.closest('label') || dqs(`label[for="${ctrl.id}"]`);
 		const onWheel = e=> {
-			if (ctrl.disabled) return;
-			e.preventDefault();
-			const [val,step,min,max]= this.vals(ctrl);
-			const sigD = String(step).split('.')[1]?.length ?? 0;
-			const cval = Number(val + (0 < e.deltaY ? -step : step)).toFixed(sigD);
-			ctrl.value = clamp(cval, min, max);
-			between(cval, min, max) && ctrl.trigger('input');
+			e.preventDefault()
+			0 < e.deltaY
+				? ctrl.stepDown()
+				: ctrl.stepUp()
+			$(ctrl).trigger('input')
 		};
 		label?.addEventListener('wheel', onWheel);
 		!ctrl.closest('label') && ctrl.addEventListener('wheel', onWheel);
