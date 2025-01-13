2c6c1d4c709068bf50809962f613c0c99e05ef6d
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-14 08:13:14 +0900

    Add files via upload

--- PACMAN-canvas/_lib/direction.js
+++ PACMAN-canvas/_lib/direction.js
@@ -4,12 +4,12 @@ export const Dir = Object.freeze(new class {
 	isValid    = (dir)=> Object.hasOwn(Dir, dir)
 	opposite   = (dir)=> OppositeMap.get(dir)
 	isOpposite = (a,b)=> this.opposite(a) == b
-	from(e, {awsd=false}={}) {
+	from(e, {wasd=false}={}) {
 		if (!isKeyboardEvent(e)) return null
 		if (isCombinationKey(e)) return null
 		const key = String(e.code).replace(/^(Arrow|Key)/,'')
 		return Dir.isValid(key)? key
-			: (awsd && DirFromAWSD.get(key.toUpperCase()) || null)
+			: (wasd && DirFromAWSD.get(key.toUpperCase()) || null)
 	}
 })
 ,OppositeMap = new Map([[ U, D],[ R, L],[ D, U],[ L, R]])
