97c2a8931922e558184ae362130369c409a79822
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-16 10:46:19 +0900

    Add files via upload

--- PACMAN-canvas/src/pacman/pac_sprite.js
+++ PACMAN-canvas/src/pacman/pac_sprite.js
@@ -1,5 +1,5 @@
 import {Ticker} from '../../_lib/timer.js'
-import {Dir}    from '../../_lib/direction.js'
+import {L}      from '../../_lib/direction.js'
 import {Ctx}    from '../_canvas.js'
 import {Losing} from './pac_losing.js'
 import {Color,PacRadius} from '../_constants.js'
@@ -7,29 +7,30 @@ import {Color,PacRadius} from '../_constants.js'
 const Duration   = 150/Ticker.Interval
 const RotateEnum = freeze({Left:0,Up:1,Right:2,Down:3})
 
-export const MouthMax = 90 * PI/180
-export const MouthMin = 20 * PI/180
-export const MouthMid = 60 * PI/180
+const Closed  = 90 * PI/180
+const MaxOpen = 20 * PI/180
+const MidOpen = 60 * PI/180
 
 export default class {
 	#losing  = null
 	#rad     =  0
 	#animDir = -1
-	constructor(obj, {closed=true}={}) {
-		this.obj  = isObj(obj) ? obj : {}
-		this.#rad = obj?.mouthRad ?? (closed? MouthMax : MouthMid)
+	constructor(obj={}) {
+		this.obj  = obj
+		this.#rad = [Closed,MidOpen,MaxOpen][obj?.opening ?? 0]
 		freeze(this)
 	}
 	get rad()        {return this.#rad}
-	get mouthAngle() {return this.obj.notPlaying? MouthMax : this.rad}
-	get orient()     {return this.obj.orient}
+	get mouthAngle() {return this.obj.notPlaying? Closed : this.rad}
+	get orient()     {return this.obj.orient ?? L}
 	set orient(dir)  {this.obj.orient = dir}
 
 	update() {
 		const {obj,rad}= this
-		if (obj.notPlaying || obj.stopped && rad < MouthMid) return
-		if (rad < MouthMin || MouthMax < rad) this.#animDir *= -1
-		this.#rad += (MouthMax - MouthMin)/Duration * this.#animDir
+		if (obj.notPlaying) return
+		if (obj.stopped  && rad < MidOpen) return
+		if (rad > Closed || rad < MaxOpen) this.#animDir *= -1
+		this.#rad += (Closed - MaxOpen)/Duration * this.#animDir
 	}
 	draw(ctx=Ctx, {x=0,y=0}={}, scale=1) {
 		const {obj}= this, Radius = (obj.Radius ?? PacRadius)*scale
@@ -39,7 +40,7 @@ export default class {
 		}
 		ctx.save()
 	 	ctx.translate(x, y)
-		ctx.rotate(RotateEnum[obj.orient] * PI/2)
+		ctx.rotate(RotateEnum[obj.orient ?? L] * PI/2)
 		ctx.beginPath()
 		ctx.moveTo(Radius*0.35, 0)
 		ctx.arc(0,0, Radius, -PI/2-this.mouthAngle, PI/2+this.mouthAngle)
