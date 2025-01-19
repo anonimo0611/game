857f5a3977550156508e115a7c1851d609c0a7da
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-20 02:09:09 +0900

    Add files via upload

--- PACMAN-canvas/src/lives.js
+++ PACMAN-canvas/src/lives.js
@@ -6,24 +6,17 @@ import {Form}  from './control.js'
 import Sprite  from './pacman/pac_sprite.js'
 import {TileSize as T} from './_constants.js'
 
-export const Lives = new class {
-	static {
-		$(Form.lvsRng).on('input', ()=> Lives.#set())
-		$on('Title Start Ready Restart', ()=> Lives.#set())
-	}
-	#left = 0
-	get left() {return Lives.#left}
-	get Max()  {return Ctrl.livesMax-1}
-	#setCurrent() {
-		const {left,Max}=Lives
-		return this.#left = +{
-			Title:  Max,
-			Start:  Max+1,
-			Ready:  State.lastIs('Start')? Max:left,
+export const Lives = function() {
+	let left = 0
+	function currentValue(max) {
+		return left = +{
+			Title:  max,
+			Start:  max+1,
+			Ready:  State.lastIs('Start')? max:left,
 			Restart:left-1,
 		}[State.current]
 	}
-	#draw(left=Lives.#setCurrent()) {
+	function set(left=currentValue(Ctrl.livesMax-1)) {
 		const sprite = new Sprite({Radius:T*.78}, 1)
 		BgCtx.save()
 		BgCtx.translate(T*2, T*32)
@@ -32,6 +25,9 @@ export const Lives = new class {
 			sprite.draw(BgCtx, Vec2(T*2*i,0).add(T))
 		BgCtx.restore()
 	}
-	#set()   {Lives.#draw()}
-	append() {Lives.#draw(++Lives.#left)}
-}
+	$(Form.lvsRng).on('input', ()=> set())
+	$on('Title Start Ready Restart', ()=> set())
+	return {
+		get left() {return left}, append() {set(++left)}
+	}
+}()
