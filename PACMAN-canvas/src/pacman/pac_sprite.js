a2ed3123ce99aad53e13572acc119331cce40982
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-20 19:13:04 +0900

    Add files via upload

--- PACMAN-canvas/src/pacman/pac_sprite.js
+++ PACMAN-canvas/src/pacman/pac_sprite.js
@@ -1,49 +1,49 @@
-import {Ticker}  from '../_lib/timer.js';
-import {U,R,D,L} from '../_lib/direction.js';
-import {Color}   from '../maze.js';
-import {Losing}  from './losing.js';
+import {Ticker}  from '../../_lib/timer.js'
+import {U,R,D,L} from '../../_lib/direction.js'
+import {Ctx}     from '../_canvas.js'
+import {Losing}  from './pac_losing.js'
+import {Color,PacRadius} from '../_constants.js'
 
-const Duration = 150/Ticker.Interval;
+const Duration = 150/Ticker.Interval
 const OpenMid  = 30 * PI/180
 const OpenMax  = 60 * PI/180
-const AngleMap = new Map([[R,0],[D,1],[L,2],[U,3]]);
+const AngleMap = new Map([[R,0],[D,1],[L,2],[U,3]])
 
-export class Sprite {
-	#obj     = null;
-	#losing  = null;
-	#rad     =  0;
-	#animDir = -1;
-	constructor(obj, {closed=true}={}) {
-		this.#obj = obj;
-		this.#rad = closed? 0 : OpenMid;
+export default class {
+	#losing  = null
+	#rad     =  0
+	#animDir = -1
+	constructor(obj={}, openType=0) {
+		this.obj  = obj
+		this.#rad = [0,OpenMid,OpenMax][openType]
+		freeze(this)
 	}
 	get angle() {
-		return (this.#obj?.stayStill ? 0 : this.#rad);
+		return this.obj.mouthClosed? 0 : this.#rad
 	}
 	update() {
-		if (this.#obj?.stayStill
-		 || this.#obj?.stopped && this.#rad > OpenMid) return;
-		 const dir = between(this.angle, 0, OpenMax) ? 1 : -1;
-		 this.#rad += OpenMax/Duration * (this.#animDir*=dir);
+		if (this.obj.stopped && this.angle > OpenMid) return
+		const dir = between(this.angle, 0, OpenMax) ? 1 : -1
+		this.#rad += OpenMax/Duration * (this.#animDir*=dir)
 	}
-	draw(ctx, {x,y}) {
-		const {orient=L,Radius,fadeIn}= this.#obj;
+	draw(ctx=Ctx, {x=0,y=0}={}, scale=1) {
+		const {obj}=this, Radius=(obj.Radius??PacRadius)*scale
 		if (this.#losing) {
-			this.#losing.draw(ctx, x,y, this.#obj);
-			return;
+			this.#losing.draw(ctx, x,y, Radius)
+			return
 		}
-		ctx.save();
-		fadeIn?.update(ctx);
-	 	ctx.translate(x, y);
-		ctx.rotate(AngleMap.get(orient) * PI/2);
-		ctx.beginPath();
-			ctx.fillStyle = Color.PacMan;
-			ctx.moveTo(-Radius*0.35, 0);
-			ctx.arc(0,0, Radius, this.angle, PI*2-this.angle);
-		ctx.fill();
-		ctx.restore();
+		ctx.save()
+	 	ctx.translate(x, y)
+		ctx.rotate(AngleMap.get(obj.orient ?? L) * PI/2)
+		ctx.beginPath()
+		ctx.moveTo(-Radius*0.35, 0)
+		ctx.arc(0,0, Radius, this.angle, PI*2-this.angle)
+		ctx.fillStyle = Color.Pacman
+		ctx.fill()
+		ctx.restore()
+		obj.showCenter && cvsFillCircle(ctx)(x, y, 3, '#F00')
 	}
 	setLosing() {
-		this.#losing = new Losing();
+		this.#losing = new Losing
 	}
 }
