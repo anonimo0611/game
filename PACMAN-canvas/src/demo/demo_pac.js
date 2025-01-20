218bb1002733aa11d605ec4129a8292f67211b9f
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-21 04:01:09 +0900

    Add files via upload

--- PACMAN-canvas/src/demo/demo_pac.js
+++ PACMAN-canvas/src/demo/demo_pac.js
@@ -1,6 +1,8 @@
-import {Actor} from '../actor.js'
-import Sprite  from '../pacman/pac_sprite.js'
+import {Actor}     from '../actor.js'
+import Sprite      from '../pacman/pac_sprite.js'
+import {PacRadius} from '../_constants.js'
 export class Pacman extends Actor {
     sprite = new Sprite(this)
+    Radius = PacRadius
     constructor() {super();freeze(this)}
 }
