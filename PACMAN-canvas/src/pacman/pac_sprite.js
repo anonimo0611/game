5dd4f6d47c147e696a67f1ce6d0bb8711482ffe2
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-20 19:12:02 +0900

    Add files via upload

--- PACMAN-canvas/src/pacman/pac_sprite.js
+++ PACMAN-canvas/src/pacman/pac_sprite.js
@@ -1,52 +1,49 @@
-import {Ticker} from '../../_lib/timer.js'
-import {L}      from '../../_lib/direction.js'
-import {Ctx}    from '../_canvas.js'
-import {Losing} from './pac_losing.js'
-import {Color,PacRadius} from '../_constants.js'
+import {Ticker}  from '../_lib/timer.js';
+import {U,R,D,L} from '../_lib/direction.js';
+import {Color}   from '../maze.js';
+import {Losing}  from './losing.js';
 
-const Duration   = 150/Ticker.Interval
-const RotateEnum = freeze({Left:0,Up:1,Right:2,Down:3})
+const Duration = 150/Ticker.Interval;
+const OpenMid  = 30 * PI/180
+const OpenMax  = 60 * PI/180
+const AngleMap = new Map([[R,0],[D,1],[L,2],[U,3]]);
 
-const Closed  = 90 * PI/180
-const OpenMax = 20 * PI/180
-const OpenMid = 60 * PI/180
-
-export default class {
-	#losing  = null
-	#rad     =  0
-	#animDir = -1
-	constructor(obj={}, openType=0) {
-		this.obj  = obj
-		this.#rad = [Closed,OpenMid,OpenMax][openType]
-		freeze(this)
+export class Sprite {
+	#obj     = null;
+	#losing  = null;
+	#rad     =  0;
+	#animDir = -1;
+	constructor(obj, {closed=true}={}) {
+		this.#obj = obj;
+		this.#rad = closed? 0 : OpenMid;
 	}
-	get mouthRad() {
-		return this.obj.mouthClosed? Closed : this.#rad
+	get angle() {
+		return (this.#obj?.stayStill ? 0 : this.#rad);
 	}
 	update() {
-		const {obj,mouthRad:rad}= this
-		if (obj.stopped  && rad < OpenMid) return
-		if (rad > Closed || rad < OpenMax) this.#animDir *= -1
-		this.#rad += (Closed - OpenMax)/Duration * this.#animDir
+		if (this.#obj?.stayStill
+		 || this.#obj?.stopped && this.#rad > OpenMid) return;
+		 const dir = between(this.angle, 0, OpenMax) ? 1 : -1;
+		 this.#rad += OpenMax/Duration * (this.#animDir*=dir);
 	}
-	draw(ctx=Ctx, {x=0,y=0}={}, scale=1) {
-		const {obj}= this, Radius = (obj.Radius ?? PacRadius)*scale
+	draw(ctx, {x,y}) {
+		const {orient=L,Radius,fadeIn}= this.#obj;
 		if (this.#losing) {
-			this.#losing.draw(ctx, x,y, Radius)
-			return
+			this.#losing.draw(ctx, x,y, this.#obj);
+			return;
 		}
-		ctx.save()
-	 	ctx.translate(x, y)
-		ctx.rotate(RotateEnum[obj.orient ?? L] * PI/2)
-		ctx.beginPath()
-		ctx.moveTo(Radius*0.35, 0)
-		ctx.arc(0,0, Radius, -PI/2-this.mouthRad, PI/2+this.mouthRad)
-		ctx.fillStyle = Color.Pacman
-		ctx.fill()
-		ctx.restore()
-		obj.showCenter && cvsFillCircle(ctx)(x, y, 3, '#F00')
+		ctx.save();
+		fadeIn?.update(ctx);
+	 	ctx.translate(x, y);
+		ctx.rotate(AngleMap.get(orient) * PI/2);
+		ctx.beginPath();
+			ctx.fillStyle = Color.PacMan;
+			ctx.moveTo(-Radius*0.35, 0);
+			ctx.arc(0,0, Radius, this.angle, PI*2-this.angle);
+		ctx.fill();
+		ctx.restore();
 	}
 	setLosing() {
-		this.#losing = new Losing
+		this.#losing = new Losing();
 	}
 }
