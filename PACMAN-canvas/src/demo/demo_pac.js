a15dff9096f09f531417da9a3707fba56b22581a
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-22 22:02:37 +0900

    Delete PACMAN-canvas/src/demo/demo_pac.js

--- PACMAN-canvas/src/demo/demo_pac.js
+++
@@ -1,8 +0,0 @@
-import {Actor}     from '../actor.js'
-import Sprite      from '../pacman/pac_sprite.js'
-import {PacRadius} from '../_constants.js'
-export class Pacman extends Actor {
-    sprite = new Sprite(this)
-    Radius = PacRadius
-    constructor() {super();freeze(this)}
-}
