eeda28bcf2bc43ca35ff852b3d1c120ac34b12c7
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-22 23:37:16 +0900

    Add files via upload

--- PACMAN-canvas/_lib/state.js
+++ PACMAN-canvas/_lib/state.js
@@ -12,21 +12,21 @@ export default class {
 			if (state) this.Enum[state] = state; else continue
 			if (this.#state === '' && val === true) this.#state = state
 			defineProperty(this, key, {get(){return this.#state === state}})
-			this['switchTo'+state] = data=> this.switchTo(state, data)
+			this['switchTo'+state] = (...args)=> this.switchTo(state, ...args)
 		}
 		freeze(this.Enum)
 		hasOwn(this.Enum, state) && this.switchTo(state)
 	}
-	switchTo(state, {delay=-1,fn}={}) {
+	switchTo(state, {delay=-1,data,fn}={}) {
 		if (!hasOwn(this.Enum, state))
 			throw ReferenceError(`State '${state}' is not defined`)
 		if (delay >= 0) {
-			Timer.set(delay, ()=> this.switchTo(state,{fn}))
+			Timer.set(delay, ()=> this.switchTo(state,{delay:-1,data}))
 			return this
 		}
 		this.#last  = this.current
 		this.#state = state
-		isFun(fn) && fn(state)
+		isFun(fn) && fn(state,data)
 		return this
 	}
 }
