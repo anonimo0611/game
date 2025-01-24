2c6b905afebfea6c0ff7c9f0a15970bfa30e607f
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-25 01:04:50 +0900

    Add files via upload

--- RandPac/src/pacman/pac_sprite.js
+++ RandPac/src/pacman/pac_sprite.js
@@ -3,28 +3,28 @@ import {U,R,D,L} from '../_lib/direction.js';
 import {Color}   from '../maze.js';
 import {Losing}  from './losing.js';
 
-const Duration = 150/Ticker.Interval;
-const OpenMid  = 30 * PI/180
-const OpenMax  = 60 * PI/180
-const AngleMap = new Map([[R,0],[D,1],[L,2],[U,3]]);
+const Duration  = 150/Ticker.Interval;
+const OpenMid   = 30 * PI/180
+const OpenMax   = 60 * PI/180
+const RotateMap = new Map([[R,0],[D,1],[L,2],[U,3]]);
 
 export class Sprite {
 	#obj     = null;
 	#losing  = null;
-	#rad     =  0;
+	#mouth   =  0;
 	#animDir = -1;
 	constructor(obj, {closed=true}={}) {
 		this.#obj = obj;
-		this.#rad = closed? 0 : OpenMid;
+		this.#mouth = closed? 0 : OpenMid;
 	}
-	get angle() {
-		return (this.#obj?.stayStill ? 0 : this.#rad);
+	get mouthAngle() {
+		return (this.#obj?.stayStill ? 0 : this.#mouth);
 	}
 	update() {
 		if (this.#obj?.stayStill
-		 || this.#obj?.stopped && this.#rad > OpenMid) return;
-		 const dir = between(this.angle, 0, OpenMax) ? 1 : -1;
-		 this.#rad += OpenMax/Duration * (this.#animDir*=dir);
+		 || this.#obj?.stopped && this.#mouth > OpenMid) return;
+		 const dir = between(this.mouthAngle, 0, OpenMax) ? 1 : -1;
+		 this.#mouth += OpenMax/Duration * (this.#animDir*=dir);
 	}
 	draw(ctx, {x,y}) {
 		const {orient=L,Radius,fadeIn}= this.#obj;
@@ -35,11 +35,11 @@ export class Sprite {
 		ctx.save();
 		fadeIn?.update(ctx);
 	 	ctx.translate(x, y);
-		ctx.rotate(AngleMap.get(orient) * PI/2);
+		ctx.rotate(RotateMap.get(orient) * PI/2);
 		ctx.beginPath();
 			ctx.fillStyle = Color.PacMan;
 			ctx.moveTo(-Radius*0.35, 0);
-			ctx.arc(0,0, Radius, this.angle, PI*2-this.angle);
+			ctx.arc(0,0, Radius, this.mouthAngle, PI*2-this.mouthAngle);
 		ctx.fill();
 		ctx.restore();
 	}
