d68e20e45b8a53cdb0b6a4bc2eec172f5266cac9
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-27 23:03:54 +0900

    Add files via upload

--- PACMAN-canvas/src/pacman/pac_sprite.js
+++ PACMAN-canvas/src/pacman/pac_sprite.js
@@ -9,19 +9,13 @@ const OpenMid   = 30 * PI/180
 const OpenMax   = 60 * PI/180
 const RotateMap = new Map([[R,0],[D,1],[L,2],[U,3]])
 
-const DefaultState = freeze({
-	orient:L, Radius:TileSize*PacScale,
-	stopped:false, mouthClosed:false, showCenter:false,
-})
-
 export default class {
 	/** @type {?Losing} */
 	#losing  = null
 	#mouth   =  0
 	#animDir = -1
-	/** @param {DefaultState} state */
-	constructor(state, openType=0) {
-		this.state  = {...DefaultState, ...state}
+	constructor(state={}, openType=0) {
+		this.state  = state ?? {}
 		this.#mouth = [0,OpenMid,OpenMax][openType]
 		freeze(this)
 	}
@@ -41,7 +35,7 @@ export default class {
 		}
 		ctx.save()
 	 	ctx.translate(x, y)
-		ctx.rotate(RotateMap.get(state.orient) * PI/2)
+		ctx.rotate(RotateMap.get(state.orient ?? L) * PI/2)
 		ctx.beginPath()
 		ctx.moveTo(-Radius*0.35, 0)
 		ctx.arc(0,0, Radius, this.mouthAngle, PI*2-this.mouthAngle)
