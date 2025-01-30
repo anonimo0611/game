a7bcdc4c8dbd8a474a34c8cd723adda394e71225
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-30 21:41:00 +0900

    Delete PACMAN-canvas/res/burger.js

--- PACMAN-canvas/res/burger.js
+++
@@ -1,8 +0,0 @@
-const s=18,lw=s/6,[e,c]=canvas2D(0,s).vals
-c.lineWidth=lw
-c.lineCap='round'
-c.strokeStyle='#FFF'
-for(let i=0;i<3;i++){
-    let y=s/9+s/8*i+s/3.5*i
-    cvsStrokeLine(c)(lw/2,y,s-lw/2,y)
-}$('#cfgBtn').empty().append(e)
