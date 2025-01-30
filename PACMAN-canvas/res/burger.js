4ac090604248f6fb535ba36a1784c91bda3d4bee
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-01-30 10:05:14 +0900

    Add files via upload

--- PACMAN-canvas/res/burger.js
+++ PACMAN-canvas/res/burger.js
@@ -1,6 +1,8 @@
-const s=18,[e,c]=canvas2D(0,s).vals
-c.lineWidth=s/6
+const s=18,lw=s/6,[e,c]=canvas2D(0,s).vals
+c.lineWidth=lw
 c.lineCap='round'
 c.strokeStyle='#FFF'
-for(let i=0;i<3;i++)cvsStrokeLine(c)(s/12,s/9+s/8*i+s/3.5*i,s-s/12,s/9+s/8*i+s/3.5*i)
-$('#cfgBtn').text('').append(e)
+for(let i=0;i<3;i++){
+    let y=s/9+s/8*i+s/3.5*i
+    cvsStrokeLine(c)(lw/2,y,s-lw/2,y)
+}$('#cfgBtn').empty().append(e)
