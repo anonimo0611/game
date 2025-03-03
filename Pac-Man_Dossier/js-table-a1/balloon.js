00c9a2f66150468e9faffdccc2ed733653650b2e
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-03-04 05:10:41 +0900

    Add files via upload

--- Pac-Man_Dossier/js-table-a1/balloon.js
+++ Pac-Man_Dossier/js-table-a1/balloon.js
@@ -1,29 +1,26 @@
-﻿/** @type {HTMLTableElement} Tbl */
-const Tbl = document.querySelector('#table-a1');
-const Bal = $('<div id="a1-balloon" class="balloon"></div>').hide().get(0);
+﻿const Tbl = document.querySelector('#table-a1')
+const Bal = $('<div id="a1-balloon" class="balloon"></div>').hide().get(0)
 
 function balloon(e) {
-	/** @type {HTMLTableCellElement} cell */
-	const cell     = e.target.closest('td');
-	const cellIdx  = cell.cellIndex;
-	/** @type {HTMLTableCellElement} rowCell */
-	const rowCell  = cell.parentNode.cells[0];
-	const colCell  = Tbl.tHead.rows[0].cells[cellIdx];
-	const firstTR  = Tbl.tBodies[0].rows[0];
-	const left     = cell.offsetLeft;
-	const top      = Tbl.offsetTop + cell.offsetTop;
-	const tableW   = Tbl.offsetWidth;
-	const cellHW   = cell.offsetWidth / 2;
-	const rowTxt   = rowCell.innerText;
-	const colTxt   = colCell.innerText;
-	const taleSize = getComputedStyle(Bal).getPropertyValue('--tale');
+	const cell     = e.target.closest('td')
+	const cellIdx  = cell.cellIndex
+	const rowCell  = cell.parentNode.cells[0]
+	const colCell  = Tbl.tHead.rows[0].cells[cellIdx]
+	const firstTR  = Tbl.tBodies[0].rows[0]
+	const tableW   = Tbl.offsetWidth
+	const top      = Tbl.offsetTop + cell.offsetTop
+	const left     = cell.offsetLeft
+	const cellHW   = cell.offsetWidth/2
+	const rowTxt   = rowCell.innerText
+	const colTxt   = colCell.innerText
+	const taleSize = getComputedStyle(Bal).getPropertyValue('--tale')
 
-	$(Bal).attr('data-side', rowTxt == '1' ? 'bottom' : 'top').show();
-	$(Bal).text(`Lv.${rowTxt}` + (cellIdx > 0 ? ` ${colTxt}` : ''));
+	$(Bal).attr('data-side', rowTxt == '1' ? 'bottom' : 'top').show()
+	$(Bal).text(`Lv.${rowTxt}` + (cellIdx > 0 ? ` ${colTxt}` : ''))
 
 	const h = (rowCell.parentNode == firstTR)
 		? `${cell.offsetHeight}px + ${taleSize}`
-		: `${-Bal.offsetHeight}px - ${taleSize}`;
+		: `${-Bal.offsetHeight}px - ${taleSize}`
 
 	$(Bal).css('top', `calc(${top}px + ${h})`);
 	if (tableW < left + Bal.offsetWidth + cell.offsetHeight) {
@@ -31,21 +28,20 @@ function balloon(e) {
 		$(Bal)
 		.css('--taleOffsetLeft',`calc(${ofstLeft}px - ${taleSize})`)
 		.css('left', 'auto')
-		.css('right','0px');
+		.css('right','0px')
 	} else {
 		$(Bal)
 		.css('--taleOffsetLeft',`calc(${cellHW - 2}px - ${taleSize})`)
 		.css('left', `${left}px`)
-		.css('right','auto');
+		.css('right','auto')
 	}
 }
-
 {
-	const ClickEv = window.ontouchstart? 'touchend' : 'mousedown';
+	const ClickEv = window.ontouchstart? 'touchend' : 'mousedown'
 	const hide = e=> {
-		!e.target.closest(`#${Tbl.id} td`) && $(Bal).hide();
+		!e.target.closest(`#${Tbl.id} td`) && $(Bal).hide()
 	}
-	Tbl.parentNode.insertBefore(Bal, Tbl.nextSibling);
-	$(Tbl, 'td').on(ClickEv, balloon);
-	$(window).on(ClickEv, hide);
+	Tbl.parentNode.insertBefore(Bal, Tbl.nextSibling)
+	$(Tbl, 'td').on(ClickEv, balloon)
+	$(window).on(ClickEv, hide)
 }
--- Pac-Man_Dossier/js-table-a1/col_hiliht.js
+++ Pac-Man_Dossier/js-table-a1/col_hiliht.js
@@ -1,30 +1,30 @@
 ﻿const Tbl = dqs('#table-a1');
 const mouseover = (e, x)=> {
-	removeClasses();
-	const target = e.target;
-	const table  = target.closest('table');
-	const thead  = table.querySelector('thead');
-	table.querySelectorAll('colgroup')[x].classList.add('hlight');
+	removeClasses()
+	const target = e.target
+	const table  = target.closest('table')
+	const thead  = table.querySelector('thead')
+	table.querySelectorAll('colgroup')[x].classList.add('hlight')
 	if (thead)
 		for (const row of thead.querySelectorAll('tr'))
 			if (row.cells[x])
-				row.cells[x].classList.add('hlight');
+				row.cells[x].classList.add('hlight')
 }
 const removeClasses = ()=> {
 	if ($('#balloon').length)
-		$('#balloon').hide();
+		$('#balloon').hide()
 
-	const hilightedThs  = dqsAll('thead td.hlight, th.hlight');
+	const hilightedThs  = dqsAll('thead td.hlight, th.hlight')
 	const hilightedCols = dqsAll('colgroup.hlight');
-	for (const ths of hilightedThs)  ths.classList.remove('hlight');
-	for (const col of hilightedCols) col.classList.remove('hlight');
+	for (const ths of hilightedThs)  ths.classList.remove('hlight')
+	for (const col of hilightedCols) col.classList.remove('hlight')
 }
 Array.from(Tbl.rows[0].cells).forEach(()=>
 	Tbl.insertBefore(document.createElement('colgroup'), Tbl.firstChild)
-);
+)
 Array.from(Tbl.rows).forEach((row, y)=>
 	Array.from(row.cells).forEach((cell, x)=>
 		cell.addEventListener('mouseover', e=> mouseover(e, x, y))
 	)
-);
-Tbl.addEventListener('mouseout', removeClasses);
+)
+Tbl.addEventListener('mouseout', removeClasses)
