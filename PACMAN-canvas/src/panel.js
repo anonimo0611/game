861aee4c531a095e2aad9a886932799a7cd533e3
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-03-08 17:19:05 +0900

    Add files via upload

--- PACMAN-canvas/src/panel.js
+++ PACMAN-canvas/src/panel.js
@@ -1,21 +1,19 @@
 import {State} from './_state.js'
-new class {
-    constructor() {
-        $on('load Start', ()=> $('.panel').hide())
-        $('.panelBtn').each((_,btn)=> {
-            $on('click',      e=> this.hide(btn,e))
-            $(btn).on('click',e=> this.show(btn,e))
-        })
-    }
-    show(btn) {
-        $('.panel').toggle()
-        $(btn).toggleClass('active',$(btn.value).is(':visible'))
-    }
-    hide(btn, e) {
-        if (!State.isTitle
-         || e.target == btn
-         || e.target.closest(btn.value))
-            return
-        $(btn.value).hide() && $(btn).removeClass('active')
-    }
+
+function show(btn) {
+	$('.panel').toggle()
+	$(btn).toggleClass('active',$(btn.value).is(':visible'))
 }
+function hide(btn, e) {
+	if (!State.isTitle
+		|| e.target == btn
+		|| e.target.closest(btn.value))
+		return
+	$(btn.value).hide() && $(btn).removeClass('active')
+}
+
+$on('load Start', ()=> $('.panel').hide())
+$('.panelBtn').each((_,btn)=> {
+	$on('click',      e=> hide(btn,e))
+	$(btn).on('click',e=> show(btn,e))
+})
