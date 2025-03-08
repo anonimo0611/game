77cc5c80c17cae3ddc77a1dc978685208b8621e7
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-03-09 03:46:28 +0900

    Add files via upload

--- PACMAN-canvas/src/_constants.js
+++ PACMAN-canvas/src/_constants.js
@@ -1,10 +1,10 @@
-const Cols = 28
-const Rows = 34
+const Cols      = 28
+const Rows      = 34
 const PacScale  = 0.9
 const GhsScale  = 1.1
-const TileSize  = +$(dRoot).css('--tile-size'), T = TileSize
+const TileSize  = +$(dRoot).css('--tile-size'), T=TileSize
 const BaseStep  = +(T/4.5).toFixed(1)
-const PacRadius = TileSize * PacScale
+const PacRadius = T*PacScale
 const GhsType   = freeze({Akabei:0,Pinky:1,Aosuke:2,Guzuta:3,Max:4})
 const GhsNames  = freeze(keys(GhsType).slice(0,-1))
 
@@ -12,40 +12,40 @@ const [Bg,Cvs,Ctx,CvsW,CvsH]= [
 	canvas2D(null ,T*Cols,T*Rows),...
 	canvas2D('cvs',T*Cols,T*Rows).vals
 ],
-PacStep = freeze({
-	Base:     BaseStep,
-	SlowBase: 0.98,
-	Eating:   this.Base * 0.86,
-	EneEat:   this.Base * 0.95,
-	Energize: this.Base * 1.10,
+PacStep = freeze(new class {
+	Base     = BaseStep
+	SlowBase = 0.98
+	Eating   = this.Base * 0.86
+	EneEat   = this.Base * 0.95
+	Energize = this.Base * 1.10
 }),
-GhsStep = freeze({
-	Base:     BaseStep  * 1.07,
-	Idle:     this.Base * 0.50,
-	GoOut:    this.Base * 0.50,
-	Fright:   this.Base * 0.60,
-	InTunnel: this.Base * 0.60,
-	Escape:   this.Base * 1.40,
-	Return:   this.Escape,
+GhsStep = freeze(new class {
+	Base     = BaseStep  * 1.07
+	Idle     = this.Base * 0.50
+	GoOut    = this.Base * 0.50
+	Fright   = this.Base * 0.60
+	InTunnel = this.Base * 0.60
+	Escape   = this.Base * 1.40
+	Return   = this.Escape
 }),
-Color = freeze({
-	Grid:      '#F00',
-	Dot:       '#FFB8AE',
-	Wall:      '#55E',
-	Door:      '#FFB8DE',
-	Pacman:    '#FF0',
-	Akabei:    '#E00',
-	Pinky:     '#F9B',
-	Aosuke:    '#0CF',
-	Guzuta:    '#FB0',
-	GhostEyes: '#33F',
-	GhostSkin: '#F4BEB0',
-	GhostPts:  '#3CF',
-	FruitPts:  '#FFB8FF',
-	Message1:  '#0FF',
-	Message2:  '#FF0',
-	Message3:  '#F00',
-	InfoTable:       freeze(['#888','#F9E']),
-	FrightBodyTable: freeze(['#36A','#FFF']),
-	FrightFaceTable: freeze(['#F9F','#F00']),
+Color = freeze(new class {
+	Grid      = '#F00'
+	Dot       = '#FFB8AE'
+	Wall      = '#55E'
+	Door      = '#FFB8DE'
+	Pacman    = '#FF0'
+	Akabei    = '#E00'
+	Pinky     = '#F9B'
+	Aosuke    = '#0CF'
+	Guzuta    = '#FB0'
+	GhostEyes = '#33F'
+	GhostSkin = '#F4BEB0'
+	GhostPts  = '#3CF'
+	FruitPts  = '#FFB8FF'
+	Message1  = '#0FF'
+	Message2  = '#FF0'
+	Message3  = '#F00'
+	InfoTable       = freeze(['#888','#F9E'])
+	FrightBodyTable = freeze(['#36A','#FFF'])
+	FrightFaceTable = freeze(['#F9F','#F00'])
 })
