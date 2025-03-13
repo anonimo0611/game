bf3b35e1196cb296cfbc9b3e205beecbd3f0a28e
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-03-13 12:44:40 +0900

    Add files via upload

--- PACMAN-canvas/src/lives.js
+++ PACMAN-canvas/src/lives.js
@@ -5,20 +5,21 @@ import Sprite  from './pacman/pac_sprite.js'
 
 export const Lives = function() {
 	let left = 0
-	function currentValue(max) {
-		return left = +{
+	function currentValue() {
+		const max = Ctrl.livesMax-1
+		return +{
 			Title:  max,
 			Start:  max+1,
 			Ready:  State.lastIs('Start')? max:left,
 			Restart:left-1,
 		}[State.current]
 	}
-	function set(left=currentValue(Ctrl.livesMax-1)) {
+	function set(val=currentValue()) {
 		const sprite = new Sprite(Bg.ctx,{openType:1})
 		Bg.ctx.save()
 		Bg.ctx.translate(T*2, T*32)
 		Bg.ctx.clearRect(0,0, T*2*5, T*2)
-		for (let i=0; i<left; i++) {
+		for (let i=0; i<(left=val); i++) {
 			const centerPos = Vec2(T*2*i,0).add(T)
 			sprite.draw({radius:T*.78,centerPos})
 		}
