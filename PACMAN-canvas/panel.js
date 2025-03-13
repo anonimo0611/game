65540431009fec927891b53a48eebc383aede416
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-03-14 05:02:37 +0900

    Add files via upload

---
+++ PACMAN-canvas/panel.js
@@ -0,0 +1,19 @@
+import {State} from './_state.js'
+
+function show(btn) {
+	$('.panel').toggle()
+	$(btn).toggleClass('active',!!$(btn.value).height())
+}
+function hide(btn, e) {
+	if (!State.isTitle
+	 || e.target == btn
+	 || e.target.closest?.(btn.value))
+		return
+	$(btn.value).hide() && $(btn).removeClass('active')
+}
+
+$on('DOMContentLoaded Start', ()=> $('.panel').hide())
+$('.panelBtn').each((_,btn)=> {
+	$on('click',      e=> hide(btn,e))
+	$(btn).on('click',e=> show(btn,e))
+})
