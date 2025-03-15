2061e29a9d07760121ded00104fd6316c30b28cc
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-03-16 04:05:14 +0900

    Add files via upload

--- PACMAN-canvas/src/_constants.js
+++ PACMAN-canvas/src/_constants.js
@@ -1,34 +1,13 @@
-const Cols      = 28
-const Rows      = 34
-const PacScale  = 0.9
-const GhsScale  = 1.1
-const TileSize  = +$(dRoot).css('--tile-size'), T = TileSize
-const BaseStep  = +(T/4.5).toFixed(1)
-const PacRadius = TileSize*PacScale
-const GhsType   = freeze({Akabei:0,Pinky:1,Aosuke:2,Guzuta:3,Max:4})
-const GhsNames  = freeze(keys(GhsType).slice(0,-1))
+const Cols = 28
+const Rows = 34
+const TileSize = +$(dRoot).css('--tile-size'), T=TileSize
+const BaseStep = +(TileSize/4.5).toFixed(1)
 
 const [Bg,Cvs,Ctx,CvsW,CvsH]= [
-	canvas2D(null ,T*Cols,T*Rows),...
-	canvas2D('cvs',T*Cols,T*Rows).vals
-],
-PacStep = freeze(new class {
-	Base     = BaseStep
-	SlowBase = 0.98
-	Eating   = this.Base * 0.86
-	EneEat   = this.Base * 0.95
-	Energize = this.Base * 1.10
-}),
-GhsStep = freeze(new class {
-	Base     = BaseStep  * 1.07
-	Idle     = this.Base * 0.50
-	GoOut    = this.Base * 0.50
-	Fright   = this.Base * 0.60
-	InTunnel = this.Base * 0.60
-	Escape   = this.Base * 1.40
-	Return   = this.Escape
-}),
-Color = freeze(new class {
+	canvas2D(null , TileSize*Cols, TileSize*Rows),...
+	canvas2D('cvs', TileSize*Cols, TileSize*Rows).vals
+]
+const Color = freeze(new class {
 	Grid      = '#F00'
 	Dot       = '#FFB8AE'
 	Wall      = '#55E'
@@ -49,4 +28,27 @@ Color = freeze(new class {
 	InfoTable       = freeze(['#888','#F9E'])
 	FrightBodyTable = freeze(['#36A','#FFF'])
 	FrightFaceTable = freeze(['#F9F','#F00'])
+})
+
+const PacScale  = 0.9
+const PacRadius = TileSize*PacScale
+const PacStep   = freeze(new class {
+	Base     = BaseStep
+	SlowBase = 0.98
+	Eating   = this.Base * 0.86
+	EneEat   = this.Base * 0.95
+	Energize = this.Base * 1.10
+})
+
+const GhsScale = 1.1
+const GhsType  = freeze({Akabei:0,Pinky:1,Aosuke:2,Guzuta:3,Max:4})
+const GhsNames = freeze(keys(GhsType).slice(0,-1))
+const GhsStep  = freeze(new class {
+	Base     = BaseStep  * 1.07
+	Idle     = this.Base * 0.50
+	GoOut    = this.Base * 0.50
+	Fright   = this.Base * 0.60
+	InTunnel = this.Base * 0.60
+	Escape   = this.Base * 1.40
+	Return   = this.Escape
 })
