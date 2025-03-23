3c25aaa5d75c38c182cdc3da5fd235ba6f34757f
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-03-24 02:23:24 +0900

    Add files via upload

--- PACMAN-canvas/_lib/confirm.js
+++ PACMAN-canvas/_lib/confirm.js
@@ -1,8 +1,13 @@
+let _opened = false
 export const Confirm = new class {
-	#opened = false
-	get opened() {return this.#opened}
+	get opened() {return _opened}
+	/**
+	 * @param {string} content
+	 * @param {function} fn1
+	 * @param {function} fn2
+	 */
 	open(content, fn1,fn2, btnStr1='Ok',btnStr2='Cancel', cancelIdx=1) {
-		if (Confirm.opened) return
+		if (_opened) return
 		this.#append()
 		const confirm = byId('confirm')
 		const buttons = confirm.querySelectorAll('button')
@@ -16,20 +21,20 @@ export const Confirm = new class {
 			btn.onkeydown = e=> {Dir.from(e)==[R,L][i] && btns[1^i].focus()}
 		})
 		confirm.showModal()
-		Confirm.#opened = true
 		$(confirm).find('.content').text(content).end().opacity(1,500)
 		$onNS(NS,'keydown',  e=> {e.key == 'Escape' && cancel(e)})
 		$onNS(NS,'mousedown',e=> {e.preventDefault()})
-	}
-	#append() {
-		const temp = byId('confirm_temp').content.cloneNode(true)
-		document.body.append(temp)
+		_opened = true
 	}
 	#close(fn) {
 		$('#confirm').opacity(0,500).on('transitionend', e=> {
 			$(e.target).remove()
 			isFun(fn) && fn()
-			this.#opened = false
+			_opened = false
 		})
+	}
+	#append() {
+		const temp = byId('confirm_temp').content.cloneNode(true)
+		document.body.append(temp)
 	}
 };const NS='.CONFIRM'
