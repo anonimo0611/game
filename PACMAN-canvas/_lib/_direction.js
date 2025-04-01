70cb4574c4ef949468b7944b0457c88b22ead6d4
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-04-01 22:05:46 +0900

    Add files via upload

--- PACMAN-canvas/_lib/_direction.js
+++ PACMAN-canvas/_lib/_direction.js
@@ -1,17 +1,17 @@
 'use strict'
 const [U,R,D,L]='Up|Right|Down|Left'.split('|')
-const Dir = freeze(new class {
-	Up=U; Right=R; Down=D; Left=L;
-	isValid    = (dir)=> hasOwn(Dir, dir)
-	opposite   = (dir)=> OppositeMap.get(dir)
-	isOpposite = (a,b)=> this.opposite(a) == b
-	from(e, {wasd=false}={}) {
-		if (!isKeyboardEvent(e)) return null
-		if (isCombinationKey(e)) return null
-		const key = String(e.code).replace(/^(Arrow|Key)/,'')
-		return Dir.isValid(key)? key
-			: (wasd && DirFromWASD.get(key) || null)
-	}
-})
-,OppositeMap = new Map([[ U, D],[ R, L],[ D, U],[ L, R]])
-,DirFromWASD = new Map([['W',U],['A',L],['S',D],['D',R]])
+const Dir = function() {
+	const WasdToDir = new Map([['W',U],['A',L],['S',D],['D',R]])
+	return freeze(new class {
+		Up=U; Right=R; Down=D; Left=L;
+		opp = new Map([[U,D],[R,L],[D,U],[L,R]])
+		/** @param {KeyboardEvent} e */
+		from(e, {wasd=false}={}) {
+			if (!isKeyboardEvent(e)) return null
+			if (isCombinationKey(e)) return null
+			const key = e.code.replace(/^(Arrow|Key)/,'')
+			return hasOwn(Dir,key)? key
+				: (wasd && WasdToDir.get(key) || null)
+		}
+	})
+}()
