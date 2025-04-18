a733b69eae521f1def28df9f54dded644d3e3a52
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-04-19 08:13:09 +0900

    Add files via upload

--- PACMAN-canvas/src/_constants.js
+++ PACMAN-canvas/src/_constants.js
@@ -1,13 +1,15 @@
-const Cols = 28
-const Rows = 34
-const TileSize = +$(dRoot).css('--tile-size'), T=TileSize
-const BaseStep = +(TileSize/4.5).toFixed(1)
+const
+Rows     = 34,
+Cols     = 28,
+TileSize = 28, T=TileSize,
+BaseStep = TileSize / 4.5,
 
-const [Bg,,Ctx,CvsW,CvsH]= [
+[Bg,,Ctx,CvsW,CvsH]= [
 	canvas2D(null , TileSize*Cols, TileSize*Rows),...
 	canvas2D('cvs', TileSize*Cols, TileSize*Rows).vals
-]
-const Color = freeze(new class {
+],
+
+Color = freeze(new class {
 	Grid      = '#F00'
 	Dot       = '#FFB8AE'
 	Wall      = '#55E'
@@ -31,22 +33,22 @@ const Color = freeze(new class {
 	InfoTable       = freeze(['#888','#F9E'])
 	FrightBodyTable = freeze(['#36A','#FFF'])
 	FrightFaceTable = freeze(['#F9F','#F00'])
-})
+}),
 
-const PacScale  = 0.9
-const PacRadius = TileSize*PacScale
-const PacStep   = freeze(new class {
+PacScale  = 0.9,
+PacRadius = TileSize*PacScale,
+PacStep   = freeze(new class {
 	Base     = BaseStep
 	SlowBase = 0.98
 	Eating   = this.Base * 0.86
 	EneEat   = this.Base * 0.95
 	Energize = this.Base * 1.10
-})
+}),
 
-const GhsScale = 1.1
-const GhsType  = freeze({Akabei:0,Pinky:1,Aosuke:2,Guzuta:3,Max:4})
-const GhsNames = freeze(keys(GhsType).slice(0,-1))
-const GhsStep  = freeze(new class {
+GhsScale = 1.1,
+GhsType  = freeze({Akabei:0,Pinky:1,Aosuke:2,Guzuta:3,Max:4}),
+GhsNames = freeze(keys(GhsType).slice(0,-1)),
+GhsStep  = freeze(new class {
 	Base     = BaseStep  * 1.07
 	Idle     = this.Base * 0.50
 	GoOut    = this.Base * 0.50
