901340a090fb82b48d92e139b3408b34795d6b07
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-04-20 18:45:50 +0900

    Add files via upload

--- PACMAN-canvas/src/fruit.js
+++ PACMAN-canvas/src/fruit.js
@@ -12,8 +12,8 @@ const IndexTable = freeze([0,1,2,2,3,3,4,4,5,5,6,6,7])
 const PointTable = freeze([100,300,500,700,1e3,2e3,3e3,5e3])
 const TargetPos  = Vec2(CvsW/2, T*18.5).freeze()
 
-const LvCounterMax  = 7
-const LvCounterRect = freeze([T*12, CvsH-T*2, T*2*LvCounterMax, T*2])
+const LvCounterCols = 7
+const LvCounterRect = freeze([T*12, CvsH-T*2, T*2*LvCounterCols, T*2])
 
 /** @type {?FadeOut} */
 let _fadeOut = null
@@ -77,7 +77,7 @@ export const Fruit = new class {
 	}
 	#setImages() {
 		const [x,y,w,h] = LvCounterRect
-		const initCount = max(Game.level-LvCounterMax, 0)
+		const initCount = max(Game.level-LvCounterCols, 0)
 		Spr.cache(Fruit.number())
 		Bg.ctx.clearRect(x,y,w,h)
 		for (let i=initCount,cols=1; i<Game.level; i++) {
