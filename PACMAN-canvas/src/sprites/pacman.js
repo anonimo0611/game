4cde526e0fda2d05b3edfa002fa5b4dc3c9c34c2
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-04-26 02:23:03 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/pacman.js
+++ PACMAN-canvas/src/sprites/pacman.js
@@ -34,15 +34,15 @@ export default class {
 		const {ctx}  = this
 		const mAngle = (closed? 0:this.#mAngle)
 		ctx.save()
-		ctx.translate(x, y)
+		ctx.translate(x,y)
 		ctx.rotate(RotateMap.get(orient) * PI/2)
 		ctx.beginPath()
 		ctx.moveTo(-radius*scale*0.35, 0)
 		ctx.arc(0,0,radius*scale, mAngle, PI*2-mAngle)
 		ctx.fillStyle = Color.Pacman
 		ctx.fill()
-			showCenter && ctx.fillCircle(0,0, 3, Color.PacCenter)
 		ctx.restore()
+		showCenter && ctx.fillCircle(x,y, 3, Color.PacCenter)
 	}
 	setLosing() {this.#losing = new Losing(this.ctx)}
 }
