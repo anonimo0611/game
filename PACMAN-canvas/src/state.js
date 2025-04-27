ae183360f29e23dde6e606b42e298e6d46eb1114
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-04-28 05:57:52 +0900

    Add files via upload

--- PACMAN-canvas/src/state.js
+++ PACMAN-canvas/src/state.js
@@ -25,16 +25,16 @@ export const State = new class extends _State {
 	get isSt_Ready() {return this.isStart || this.isReady}
 
 	/** @param {StateType} state */
-	#callback(state, data) {
-		Ticker.resetCount()
-		$trigger(document.body.dataset.state=state, data)
+	last(state) {
+		return super.last(state)
 	}
 	/** @param {StateType} state */
 	to(state, {delay=(state=='Quit' ? -1:0),data}={}) {
 		return super.to(state, {delay,data,fn:this.#callback})
 	}
 	/** @param {StateType} state */
-	last(state) {
-		return super.last(state)
+	#callback(state, data) {
+		Ticker.resetCount()
+		$trigger(document.body.dataset.state=state, data)
 	}
 }
