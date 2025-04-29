82880b2c4611c26771a77daae3398755053475ba
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-04-30 01:51:26 +0900

    Add files via upload

--- PACMAN-canvas/_lib/_extjq.js
+++ PACMAN-canvas/_lib/_extjq.js
@@ -5,7 +5,8 @@ $.fn.offon = function(type, ...args) {
 $.fn.opacity = function(a, ms) {
 	return !arguments.length
 		? $(this).css('opacity')
-		: $(this).css({opacity:+a,transition:isNum(ms)?`opacity ${ms}ms`:null})
+		: $(this).css({opacity:+a,
+			transition:isNum(ms)?`opacity ${ms}ms`:null})
 }
 /** @param {string} id */
 const $byId = id=> $('#'+id)
@@ -19,11 +20,10 @@ const $onNS = (ns,cfg)=> {
 		$offon(String(ev).trim().replace(/[_\s]+|$/g,`${ns}\x20`),fn))
 	return $(window)
 }
-const [$ready,$one,$on,$off,$offon,$trigger]= function() {
-	const rep = arg=> isStr(arg)? arg.trim().replace(/_/g,'\x20'):arg
-	return 'ready|one|on|off|offon|trigger'.split('|').map(f=> (...a)=> {
-		if (!isObj(a[0])) return $(window)[f](rep(a[0]),...a.slice(1))
-		entries(a[0]).forEach(([k,v])=> $(window)[f](rep(k),v))
-		return $(window)
+const [$ready,$on,$off,$offon,$trigger]=(_=> {
+	const w=this,rep=arg=>isStr(arg)?arg.trim().replace(/_/g,' '):arg
+	return['ready','on','off','offon','trigger'].map(f=>(...a)=>{
+		if(!isObj(a[0]))return $(w)[f](rep(a[0]),...a.slice(1))
+		entries(a[0]).forEach(([k,v])=>$(w)[f](rep(k),v));return $(w)
 	})
-}()
+})()
