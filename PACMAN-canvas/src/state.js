3c34215cdd3b0d7c23c990852c5b503c94987be5
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-04-29 21:27:37 +0900

    Add files via upload

--- PACMAN-canvas/src/state.js
+++ PACMAN-canvas/src/state.js
@@ -1,3 +1,5 @@
+/** @typedef {'Title'|'Attract'|'Start'|'Restart'|'NewLevel'|'Ready'|'Playing'|'Clear'|'FlashMaze'|'CoffBrk'|'Crashed'|'Losing'|'GameOver'|'Quit'} StateType */
+
 import _State from '../_lib/state.js'
 export const State = new class extends _State {
 	isTitle     = true
@@ -18,19 +20,19 @@ export const State = new class extends _State {
 		super()
 		this.init()
 	}
-	/** @returns {keyof StateType} */
+	/** @returns {StateType} */
 	get current()    {return super.current}
 	get isSt_Ready() {return this.isStart || this.isReady}
 
-	/** @param {keyof StateType} state */
+	/** @param {StateType} state */
 	last(state) {
 		return super.last(state)
 	}
-	/** @param {keyof StateType} state */
+	/** @param {StateType} state */
 	to(state, {delay=(state=='Quit' ? -1:0),data}={}) {
 		return super.to(state, {delay,data,fn:this.#callback})
 	}
-	/** @param {keyof StateType} state */
+	/** @param {StateType} state */
 	#callback(state, data) {
 		Ticker.resetCount()
 		$trigger(document.body.dataset.state=state, data)
