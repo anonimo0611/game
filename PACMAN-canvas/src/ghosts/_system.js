51c5d918e4c2c1664e47dc95a3953755967d1332
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-05-15 12:27:22 +0900

    Add files via upload

--- PACMAN-canvas/src/ghosts/_system.js
+++ PACMAN-canvas/src/ghosts/_system.js
@@ -227,7 +227,7 @@ class FrightMode {
 	static #timeList = freeze([6,5,4,3,2,5,2,2,1,5,2,1,0]) // secs
 	static get instance() {return this.#instance}
 	static get numOfSec() {return this.#timeList[Game.clampedLv-1]}
-	static start() {State.isAttract || this.numOfSec && new this()}
+	static start() {(State.isAttract||this.numOfSec) && new this()}
 
 	#tCounter = 0; #fCounter  = 0;
 	#flashIdx = 1; #caughtCnt = 0;
