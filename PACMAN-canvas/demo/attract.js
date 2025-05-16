ea55de869832197126e73a3ab65d31cde94ea5a4
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-05-16 14:34:02 +0900

    Add files via upload

---
+++ PACMAN-canvas/demo/attract.js
@@ -0,0 +1,150 @@
+﻿import {State}    from '../state.js'
+import {Ctrl}     from '../control.js'
+import {Score}    from '../score.js'
+import {PtsMgr}   from '../points.js'
+import {Fruit}    from '../fruit.js'
+import {drawDot}  from '../maze.js'
+import {drawText} from '../message.js'
+import {Pacman}   from '../pacman.js'
+import {GhsMgr}   from '../ghosts/_system.js'
+import {Ghost}    from '../ghosts/ghost.js'
+import {RunTimer} from './_run_timer.js'
+
+const CHAR = 0
+const DEMO = 1
+
+/** @type {?Attract} */
+let _attract = null
+
+export class Attract {
+	static {
+		$(RunTimer)  .on({begin:this.#begin})
+		$('.DemoBtn').on({click:this.#begin})
+		$on({Attract:()=> _attract = new Attract()})
+	}
+	static #begin() {
+		State.to('Attract')
+	}
+	static update() {
+		RunTimer .update()
+		_attract?.update()
+	}
+	static draw() {
+		_attract?.draw()
+		return State.isAttract
+	}
+	/** @type {Ghost[][]} */
+	ghsList = [[],[]]
+	pacman  = new Pacman
+	powDisp = 1
+	pacVelX = -CvsW/180
+	ghsVelX = -CvsW/169
+
+	/** @private */
+	constructor() {
+		$onNS('.Attract', {click_keydown_blur:this.quit})
+		this.initialize()
+	}
+	initialize() {
+		const {Max}= GhsType
+		for (let i=0; i<this.ghsList.length*Max; i++) {
+			this.setActor(i/Max|0, i%Max)
+		}
+		$(GhsMgr).trigger('Init', this.ghsList[DEMO])
+	}
+	/**
+	 * @param {number} idx
+	 * @param {number} gIdx
+	 */
+	setActor(idx, gIdx) {
+		const g = new Ghost(idx? L:R, {idx:gIdx,animFlag:+(idx==DEMO)})
+		if (idx) {
+			g.pos = Vec2(CvsW+(T*6)+(T*2*gIdx), T*19)
+			!gIdx && (this.pacman.pos = Vec2(g.x-T*3.5, g.y))
+		}
+		this.ghsList[idx].push(g)
+	}
+	/**
+	 * @param {number}  idx
+	 * @param {number}  ghsIdx
+	 * @param {Vector2} [pos]
+	 */
+	drawGhost(idx, ghsIdx, pos) {
+		const ghost = this.ghsList[idx][ghsIdx]
+		pos && (ghost.pos = pos)
+		ghost.sprite.draw(ghost)
+	}
+	draw() {
+		const et = Ticker.elapsedTime/100, ptsFontSize = T*.68
+		Score.draw(),drawText(7, 5, null, 'CHARACTOR　/　NICKNAME')
+		et > 10 && this.drawGhost(CHAR, 0, Vec2(5*T, 6*T))
+		et > 15 && drawText( 8,  7, Color.Akabei, 'OIKAKE----')
+		et > 20 && drawText(18,  7, Color.Akabei, '"AKABEI"')
+
+		et > 30 && this.drawGhost(CHAR, 1, Vec2(5*T, 9*T))
+		et > 35 && drawText( 8, 10, Color.Pinky, 'MACHIBUSE--')
+		et > 40 && drawText(19, 10, Color.Pinky, '"PINKY"')
+
+		et > 50 && this.drawGhost(CHAR, 2, Vec2(5*T, 12*T))
+		et > 55 && drawText( 8, 13, Color.Aosuke, 'KIMAGURE--')
+		et > 60 && drawText(18, 13, Color.Aosuke, '"AOSUKE"')
+
+		et > 70 && this.drawGhost(CHAR, 3, Vec2(5*T, 15*T))
+		et > 75 && drawText( 8, 16, Color.Guzuta, 'OTOBOKE---')
+		et > 80 && drawText(18, 16, Color.Guzuta, '"GUZUTA"')
+		if (et > 85) {
+			drawDot(Ctx, Vec2(10, 24))
+			this.powDisp && drawDot(Ctx, Vec2(10, 26), true)
+			drawText(12.0, 25, null, '10')
+			drawText(14.3, 25, null, 'PTS', {size:ptsFontSize})
+			drawText(12.0, 27, null, '50')
+			drawText(14.3, 27, null, 'PTS', {size:ptsFontSize})
+		}
+		if (et > 90) {
+			if (this.pacman.dir == L && this.powDisp) {
+				drawDot(Ctx, Vec2(4, 19), true)
+			}
+			if (Ctrl.extendPts > 0) {
+				const {BonusText:color}= Color
+				drawText( 2.0, 30, color, `BONUS　PACMAN　FOR　${Ctrl.extendPts}`)
+				drawText(24.3, 30, color, 'PTS', {size:ptsFontSize})
+			}
+		}
+		if (et > 105) {
+			for (let i=0; i<GhsType.Max; i++)
+				this.drawGhost(DEMO, i)
+			this.pacman.sprite.draw(this.pacman)
+			PtsMgr.drawFront()
+		}
+		Fruit.drawLevelCounter()
+	}
+	update() {
+		if (Ticker.elapsedTime <= 1e4+500)
+			return
+		this.powDisp ^= +(Ticker.count % 15 == 0)
+		!Timer.frozen && this.updatePacman()
+		!Timer.frozen && this.updateGhosts()
+	}
+	updatePacman() {
+		this.pacman.sprite.update()
+		this.pacman.x += this.pacVelX
+		if (this.pacman.dir == L && this.pacman.x <= T*4) {
+			this.pacVelX *= -1.11
+			this.ghsVelX /= -2.14
+			this.pacman.dir = R
+			GhsMgr.setFrightMode()
+		}
+	}
+	updateGhosts() {
+		for (const g of this.ghsList[DEMO]) {
+			g.x += this.ghsVelX
+			g.crashWithPac({pos:this.pacman.pos, radius:T/4,
+				release:()=> GhsMgr.caughtAll && Attract.#begin()})
+		}
+	}
+	quit() {
+		_attract = null
+		State.to('Title')
+		$off('.Attract')
+	}
+}
---
+++ PACMAN-canvas/maze.js
@@ -0,0 +1,155 @@
+import {Rect}   from '../_lib/rect.js'
+import {State}  from './state.js'
+import {Ghost}  from './ghosts/ghost.js'
+import {powChk} from './control.js'
+
+const MapArr = freeze([... `\
+////////////////////////////\
+A============21============B\
+#............||............#\
+#.1--2.1---2.||.1---2.1--2.#\
+#O|  |.|   |.||.|   |.|  |O#\
+#.4--3.4---3.43.4---3.4--3.#\
+#..........................#\
+#.1--2.12.1------2.12.1--2.#\
+#.4--3.||.4--21--3.||.4--3.#\
+#......||....||....||......#\
+D____b.|4--2 || 1--3|.a____C\
+     #.|1--3 43 4--2|.#     \
+     #.||          ||.#     \
+     #.|| XXXXXXXX ||.#     \
+=====c.43 X      X 43.d=====\
+      .   X      X   .      \
+_____b.12 X      X 12.a_____\
+     #.|| XXXXXXXX ||.#     \
+     #.||          ||.#     \
+     #.|| 1------2 ||.#     \
+A====c.43 4--21--3 43.d====B\
+#............||............#\
+#.1--2.1---2.||.1---2.1--2.#\
+#.4-2|.4---3.43.4---3.|1-3.#\
+#O..||.......  .......||..O#\
+4-2.||.12.1------2.12.||.1-3\
+1-3.43.||.4--21--3.||.43.4-2\
+#......||....||....||......#\
+#.1----34--2.||.1--34----2.#\
+#.4--------3.43.4--------3.#\
+#..........................#\
+D__________________________C\
+////////////////////////////\
+////////////////////////////`])
+
+/** @typedef {number} TileIdx */
+const WallSet  = /**@type {Set<TileIdx>}*/(new Set)
+const DotSet   = /**@type {Set<TileIdx>}*/(new Set)
+const PowMap   = /**@type {Map<TileIdx,Vector2>}*/(new Map)
+const DotChip  = new Set(['.','O'])
+const PenRect  = new Rect(10,13, 7,4)
+const PenOuter = new Rect( 9,12, 9,6)
+
+class House {
+	get EntranceTile() {return Vec2(13, 12)}
+	/** @param {Vector2} tilePos */
+	isIn = tilePos=> PenRect.contains(tilePos)
+	MiddleY = (this.EntranceTile.y+3.5)*T
+}
+class PowDot {
+	#disp = 1
+	/** @param {Vector2} pos */
+	#draw(pos) {
+		if (!State.isPlaying
+		 || Ticker.paused
+		 || this.#disp)
+			Maze.drawDot(Ctx, pos, true)
+	}
+	draw() {
+		this.#disp ^= +(Ticker.count % 15 == 0)
+		for (const [,pos] of PowMap) this.#draw(pos)
+	}
+}
+class Tunnel {
+	entranceL =  5.5
+	entranceR = 22.5
+	/**
+	  * @param {Vector2} centerPos
+	  * @param {'Left'|'Right'} [dir]
+	  */
+	isIn(centerPos, dir) {
+		const where = this.#where(centerPos)
+		if (dir == L) return (where == L)
+		if (dir == R) return (where == R)
+		return (where != null)
+	}
+	/** @param {Vector2} centerPos */
+	#where({x, y}) {
+		if (int(y/T) == 15 && x/T <= this.entranceL) return L
+		if (int(y/T) == 15 && x/T >= this.entranceR) return R
+		return null
+	}
+}
+
+export const Maze = new class {
+	static {$ready(this.setup)}
+	static setup() {
+		for (const [i,c] of MapArr.entries())
+			!DotChip.has(c) && c.trim() && WallSet.add(i)
+		$on({Title_NewLevel: Maze.#reset})
+		$(powChk).on({change:Maze.#reset})
+	}
+	/** @param {Event} e */
+	#reset(e) {
+		for (const [i,c] of MapArr.entries())
+			DotChip.has(c) && Maze.#setDot(i,c)
+		e.target != powChk && Maze.#drawDoor()
+	}
+	/**
+	 * @param {TileIdx} idx
+	 * @param {string}  chip
+	 */
+	#setDot(idx, chip) {
+		const v = Vec2(idx%Cols, idx/Cols|0)
+		Maze.clearBgDot({tileIdx:idx,tilePos:v})
+		DotSet.add(idx)
+		!powChk.checked || chip == '.'
+			? drawDot(Bg.ctx, v)
+			: PowMap.set(idx, v)
+	}
+	get dotsLeft() {return DotSet.size}
+	Map    = MapArr
+	DotMax = MapArr.filter(c=> DotChip.has(c)).length
+	House  = freeze(new House)
+	PowDot = freeze(new PowDot)
+	Tunnel = freeze(new Tunnel)
+
+	// These tiles(x-y) forbidden ghosts from entering upward
+	GhostNotEnterSet = new Set(['12-11','12-23','15-11','15-23'])
+
+	hasDot  = (/**@type {TileIdx} */i)=> DotSet.has(i)
+	hasPow  = (/**@type {TileIdx} */i)=> PowMap.has(i)
+	hasWall = (/**@type {Position}*/p)=> WallSet.has(p.y*Cols+p.x)
+
+	/** @param {Ghost} ghost */
+	ghostExitTile = ({originalTargetTile:o, tilePos:t})=>
+		o.y < 10 && PenOuter.contains(t)
+			? Vec2((t.x > Cols/2) && (o.x > Cols/2) ? 21:6, 15) : o
+
+	/** @param {{tileIdx:TileIdx, tilePos:Vector2}} tile */
+	clearBgDot({tileIdx:idx,tilePos:v}) {
+		DotSet.delete(idx)
+		PowMap.delete(idx)
+		drawDot(Bg.ctx, v, true, null)
+		return DotSet.size
+	}
+	/**
+	 * @param {ExtendedContext2D} ctx
+	 * @param {Position}  tilePos
+	 * @param {boolean}   isLarge
+	 * @param {?CtxStyle} color
+	 */
+	drawDot(ctx, {x,y}, isLarge=false, color=Color.Dot) {
+		ctx.fillCircle(x*T+T/2, y*T+T/2, T/(isLarge? 2:8), color)
+	}
+	#drawDoor() {
+		Bg.ctx.fillRect(13*T, 13.6*T, T*2, T/4, Color.Door)
+	}
+}, {drawDot}=freeze(Maze)
