62a0efa7a7ee7641124ee7833a8874250fde5cba
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-05-27 00:58:10 +0900

    Update _constants.js

--- PACMAN-canvas/src/_constants.js
+++ PACMAN-canvas/src/_constants.js
@@ -10,7 +10,7 @@ T = TileSize,
 /** Blinking interval in frames */
 PowDotInterval = 15,
 
-/** @typedef {'Up'|'Right'|'Down'|'Left'} Direction */
+/** @typedef {U|R|D|L} Direction */
 U='Up', R='Right', D='Down', L='Left',
 
 dBody = /**@type {HTMLBodyElement}*/(document.body),
