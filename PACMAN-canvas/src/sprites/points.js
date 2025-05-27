1dc27d244f9b4e0e943e95514c1b454f9ccbf167
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-05-27 10:39:36 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/points.js
+++ PACMAN-canvas/src/sprites/points.js
@@ -11,8 +11,8 @@ PathFrom0To8 = /**@type {const}*/({
 	7: [0,1,0,0,4,0,4,1,2,4,2,6],
 	8: [1,0,3,0,4,1,4,2,3,3,1,3,0,4,0,5,1,6,3,6,4,5,4,4,3,3,1,3,0,2,0,1],
 }),
-/** @type {{[pts:number]: [ZeroToEight, x:number, y:number][]}} */
-RelPositionsFrom = ({
+/** @typedef {keyof RelPosListFrom} PtsType */
+RelPosListFrom = /**@type {const}*/({
 	 100: [[1,-6.1,-3],[0,-2.1,-3],[0,2.7,-3]],
 	 200: [[2,-7.0,-3],[0,-1.0,-3],[0,4.0,-3]],
 	 300: [[3,-7.2,-3],[0,-1.2,-3],[0,3.8,-3]],
@@ -40,7 +40,8 @@ export default new class {
 		Ctx.strokeStyle = GhsPtsSet.has(pts)
 			? Color.GhostPts
 			: Color.FruitPts
-		RelPositionsFrom[pts]?.forEach(([n,x,y],i)=> {
+		RelPosListFrom[/**@type {PtsType}*/(pts)]
+		?.forEach(([n,x,y],i)=> {
 			pts == 1600 && i == 0
 				? this.#strokeLines([x,y,x,y+6]) // narrow 1
 				: this.#strokeNumber(n,x,y)
