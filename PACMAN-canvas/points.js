d12c36aa39e35eb5ee2aee46bbc85a58a662ff3e
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-06-05 03:59:50 +0900

    Add files via upload

---
+++ PACMAN-canvas/points.js
@@ -0,0 +1,54 @@
+import {Game}   from './_main.js'
+import {State}  from './state.js'
+import {Score}  from './score.js'
+import {GhsMgr} from './ghosts/_system.js'
+import {Fruit}  from './fruit.js'
+import * as Spr from './sprites/points.js'
+
+const PtsMap = /**@type {Map<any, Points>}*/(new Map)
+$on({Title_Clear_Crashed:()=> PtsMap.clear()})
+
+export const PtsMgr = new class {
+	/**
+	 * @param {PtsData}  data
+	 * @param {Function} [fn]
+	 */
+	set(data,fn)   {new Points(data,fn)}
+	update()       {PtsMap.forEach(v=> v.update())}
+	drawFruitPts() {PtsMap.get(Fruit) ?.draw()}
+	drawGhostPts() {PtsMap.get(GhsMgr)?.draw()}
+}
+class Points {
+	/**
+	 * @typedef {import('sprites/points.js').PtsType} PtsType
+	 * @typedef {{key:{score:PtsType}, x:number, y:number, duration?:number}} PtsData
+	 * @param {PtsData}  cfg
+	 * @param {Function} [fn]
+	 */
+	constructor({key,x,y,duration:dur=1e3}, fn) {
+		const spd  = Game.speedRate, fadeDur = 300
+		this.cache = Spr.cache(key.score)
+		this.pos   = Vec2(x, y)
+		this.score = key.score
+		this.fade  = new FadeOut(fadeDur/spd, (dur-fadeDur)/spd)
+		Timer.set(dur/spd, ()=> {
+			Timer.unfreeze()
+			PtsMap.delete(key)
+			fn?.()
+		})
+		State.isPlaying && Score.add(key.score)
+		PtsMap.set(key, this)
+	}
+	update() {
+		this.fade.update()
+	}
+	draw() {
+		const sideOfst = T*1.25
+		const {pos:{x,y},cache:{ctx,w,h}}= this
+		Ctx.save()
+		Ctx.setAlpha(this.fade?.alpha)
+		Ctx.translate(clamp(x, sideOfst, CvsW-sideOfst), y)
+		Ctx.drawImage(ctx.canvas, -w/2,-h/2)
+		Ctx.restore()
+	}
+}
