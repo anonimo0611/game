fd5fc02a726a98730981bc34726cf76a507b5bca
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-06-07 00:11:03 +0900

    Add files via upload

--- PACMAN-canvas/_sprites/_constants.js
+++ PACMAN-canvas/_sprites/_constants.js
@@ -2,14 +2,13 @@ import {PvC}   from './anime.js'
 import {View}  from './_main.js'
 import {Ghost} from './actor.js'
 
-export let T=0,[S,Gap]=[0,0]
+export let T=0,[S,Gap]=[0,0]; let lastT=T
 export let ghost = new Ghost(0)
 
 export const GridSize = Vec2(10,11).freeze()
 export const sizeRng  = /**@type {HTMLInputElement}*/(byId('sizeRng'))
 
-let lastT = T
-function resize() {
+export function resize() {
 	T = sizeRng.valueAsNumber/2
 	if (lastT != T) {
 		[S,Gap]=[T*2,T*.25], ghost=new Ghost(T*2)
@@ -17,4 +16,4 @@ function resize() {
 		PvC.resize(T*3, T*2) && View.draw()
 		lastT = T
 	}
-} Ticker.set(resize)
+}
