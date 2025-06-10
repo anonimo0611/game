849262b86705b19af100687622513cc689ce6e8d
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-06-10 19:20:33 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/fruits.js
+++ PACMAN-canvas/src/sprites/fruits.js
@@ -43,11 +43,10 @@ function strawberry(ctx=Ctx) {
 	ctx.fill()
 	ctx.stroke()
 
-	;/**@type {xyList[]}*/
-	([//white spots
+	;([//white spots
 		[-4,-1],[-3,2],[-2, 0],[-1,4],[0, 2],
 		[ 0, 0],[ 2,4],[ 2,-1],[ 3,1],[4,-2]
-	]).forEach(s=> ctx.fillCircle(...s, .55, '#FFF'))
+	]).forEach(([x,y])=> ctx.fillCircle(x,y, .55, '#FFF'))
 
 	// green leaf
 	ctx.newLinePath([0,-4],[-3,-4],[0,-4],[-2,-3],[-1,-3],[0,-4])
@@ -146,11 +145,10 @@ function melon(ctx=Ctx) {
 	ctx.strokeStyle = '#5B8F8C'
 	ctx.stroke()
 
-	;/**@type {xyList[]}*/
-	([//white spots
+	;([//white spots
 		[ 0.0,-2.3],[-2,-1.2],[-4, 0.8],[-3.6, 3.2],[1, 0],
 	  	[-1.3, 2.0],[-1, 4.5],[ 3, 2.5],[ 1.0, 4.5]
-	]).forEach(s=> ctx.fillCircle(...s, 0.5, '#FFF'))
+	]).forEach(([x,y])=> ctx.fillCircle(x,y, 0.5, '#FFF'))
 }
 function gala(ctx=Ctx) {
 	const yellow = '#F8FF00'
