ca068aebafddc05d1431b06c713d1f9154990e2e
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-06-26 13:10:50 +0900

    Delete PACMAN-canvas/_sprites directory

--- PACMAN-canvas/_sprites/actor.js
+++
@@ -1,9 +0,0 @@
-import GhsSprite   from '../src/sprites/ghost.js'
-import GhsSpriteCb from '../src/sprites/ghost_cb.js'
-export class Ghost {
-	constructor(/**@type {number}*/size) {
-		this.size     = size
-		this.cbSprite = GhsSpriteCb
-		this.sprite   = new GhsSprite(canvas2D(null, size*4, size*3).ctx)
-	}
-}
