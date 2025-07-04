194b48b312e14f8cd8df6d08c733b4d96c03225a
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-07-05 04:49:18 +0900

    Update wall.js

--- PACMAN-canvas/src/sprites/wall.js
+++ PACMAN-canvas/src/sprites/wall.js
@@ -15,7 +15,7 @@ export const Wall = new class {
 		ctx.restore()
 	}
 	#drawHouse() {
-		const [hl, ix, iy, ox, oy]= [LW/2, 3.1, 1.6, 3.4, 1.9]
+		const [hl,ix,iy,ox,oy]= [LW/2, 3.1, 1.6, 3.4, 1.9]
 		ctx.translate(CW/2, Maze.House.MiddleY)
 		ctx.newLinePath(
 			[-T-hl, -oy*T],[-ox*T, -oy*T],[-ox*T, +oy*T],
