da6ce0eed2d66ebafa985b0766ef9c32d1a44181
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-07-15 01:23:07 +0900

    Add files via upload

--- PACMAN-canvas/_lib/_jquery.custom.d.ts
+++ PACMAN-canvas/_lib/_jquery.custom.d.ts
@@ -1,3 +1,6 @@
 interface JQuery {
-  offon(events:string, handler:Function): JQuery;
+	offon<TType extends string>(
+		events: TType,
+		handler:JQuery.TypeEventHandler<TElement, undefined, TElement, TElement, TType>
+	):this;
 }
