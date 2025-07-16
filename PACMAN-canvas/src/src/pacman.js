3e0bb2f2d2f97f70738c423f8ccadc396ed67625
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-07-16 19:08:25 +0900

    Delete PACMAN-canvas/src/src directory

--- PACMAN-canvas/src/src/pacman.js
+++
@@ -1,9 +0,0 @@
-import {Actor} from './actor.js'
-import Sprite  from './sprites/pacman.js'
-export class Pacman extends Actor {
-	#sprite = new Sprite(Ctx)
-	get radius()  {return PacRadius}
-	get hidden()  {return Timer.frozen}
-	get sprite()  {return this.#sprite}
-	constructor() {super()}
-}
