db5564dc84ed6c6ce8cfb0200e312884370c7a68
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-08-09 00:39:35 +0900

    Add files via upload

--- PACMAN-canvas/src/_constants.js
+++ PACMAN-canvas/src/_constants.js
@@ -1,77 +1,51 @@
-const
-Rows     = 34,
-Cols     = 28,
-TileSize = 27,
-DotScore = 10,
-PowScore = 50,
-MoveStep = TileSize/4.5,
+const Cols      = 28
+const Rows      = 34
+const PacScale  = 0.9
+const GhsScale  = 1.1
+const TileSize  = +$(dRoot).css('--tile-size'), T=TileSize
+const BaseStep  = +(T/4.5).toFixed(1)
+const PacRadius = T*PacScale
+const GhsType   = freeze({Akabei:0,Pinky:1,Aosuke:2,Guzuta:3,Max:4})
+const GhsNames  = freeze(keys(GhsType).slice(0,-1))
 
-/** Shorthand of TileSize */
-T = TileSize,
-
-/** Blinking interval in frames */
-PowDotInterval = 15,
-
-/** @typedef {'Up'|'Right'|'Down'|'Left'} Direction */
-U='Up', R='Right', D='Down', L='Left',
-
-/** The document body */
-dBody = document.body,
-
-/** Board width */
-BW = T*Cols,
-/** Board height */
-BH = T*Rows,
-
-Bg  = canvas2D('board_bg' ,  BW,BH),
-HUD = canvas2D('board_hud',  BW,BH),
-Ctx = canvas2D('board_main', BW,BH).ctx,
-
-Color = freeze(new class {
+const [Bg,Cvs,Ctx,CvsW,CvsH]= [
+	canvas2D(null ,T*Cols,T*Rows),...
+	canvas2D('cvs',T*Cols,T*Rows).vals
+]
+const PacStep = freeze(new class {
+	Base     = BaseStep
+	SlowBase = 0.98
+	Eating   = this.Base * 0.86
+	EneEat   = this.Base * 0.95
+	Energize = this.Base * 1.10
+})
+const GhsStep = freeze(new class {
+	Base     = BaseStep  * 1.07
+	Idle     = this.Base * 0.50
+	GoOut    = this.Base * 0.50
+	Fright   = this.Base * 0.60
+	InTunnel = this.Base * 0.60
+	Escape   = this.Base * 1.40
+	Return   = this.Escape
+})
+const Color = freeze(new class {
 	Grid      = '#F00'
 	Dot       = '#FFB8AE'
-	Wall      = '#58E'
-	FlashWall = '#FFF'
+	Wall      = '#55E'
 	Door      = '#FFB8DE'
 	Pacman    = '#FF0'
 	Akabei    = '#E00'
 	Pinky     = '#F9B'
 	Aosuke    = '#0CF'
 	Guzuta    = '#FB0'
-	PacCenter = '#F00'
 	GhostEyes = '#33F'
 	GhostSkin = '#F4BEB0'
 	GhostPts  = '#3CF'
-	FruitPts  = '#F8F'
-	Extend    = '#F55'
+	FruitPts  = '#FFB8FF'
 	Message1  = '#0FF'
 	Message2  = '#FF0'
 	Message3  = '#F00'
-	BonusText = '#F90'
-	InfoTexts    = freeze(['#888','#F9E'])
-	FrightFaces  = freeze(['#F9F','#F00'])
-	FrightBodies = freeze(['#36A','#FFF'])
-}),
-
-PacScale  = 0.9,
-PacRadius = T*PacScale,
-PacStep   = freeze(new class {
-	SlowLevel = 13   // After this level, Pacman slows down
-	SlowRate  = 0.98 // Deceleration rate at SlowLevel
-	Base      = MoveStep
-	Eating    = this.Base * 0.86
-	Energized = this.Base * 1.10
-	EneEat    = this.Base * 0.95 // Energized+Eating
-}),
-
-GhsScale = 1.1,
-GhsType  = /**@type {const}*/({Akabei:0,Pinky:1,Aosuke:2,Guzuta:3,Max:4}),
-GhsNames = /**@type {const}*/(['Akabei','Pinky','Aosuke','Guzuta']),
-GhsStep  = freeze(new class {
-	Base     = MoveStep  * 1.07
-	Idle     = this.Base * 0.50
-	GoOut    = this.Base * 0.50
-	Fright   = this.Base * 0.60
-	InTunnel = this.Base * 0.60
-	Escape   = this.Base * 1.40
+	InfoTable       = freeze(['#888','#F9E'])
+	FrightBodyTable = freeze(['#36A','#FFF'])
+	FrightFaceTable = freeze(['#F9F','#F00'])
 })
