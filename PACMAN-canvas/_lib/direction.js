93ed2db2a79a19242ca65b69beedc60b2d797a40
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-08-19 04:34:39 +0900

    Add files via upload

--- PACMAN-canvas/_lib/direction.js
+++ PACMAN-canvas/_lib/direction.js
@@ -1,18 +1,22 @@
-/** @typedef {'Up'|'Right'|'Down'|'Left'} Direction */
 const U='Up', R='Right', D='Down', L='Left'
 const DirSet  = new Set([U,R,D,L])
 const wasdMap = new Map([['W',U],['A',L],['S',D],['D',R]])
 
-export const Dir = freeze(new class {
-	Up=U; Right=R; Down=D; Left=L;
+export const Dir = new class {
+	/** @readonly */Up    = U;
+	/** @readonly */Right = R;
+	/** @readonly */Down  = D;
+	/** @readonly */Left  = L;
+
+	/** @readonly */
 	Opposite = freeze({[U]:D, [R]:L, [D]:U, [L]:R})
 
 	/** @param {KeyboardEvent|JQuery.KeyboardEventBase} e */
 	from(e, {wasd=false}={}) {
 		if (isCombiKey(e)) return null
 		const k = e.code.replace(/^(Arrow|Key)/,'')
-		return /**@type {?Direction}*/(
+		return /**@type {?('Up'|'Right'|'Down'|'Left')}*/(
 			DirSet.has(k)? k : (wasd && wasdMap.get(k)) || null
 		)
 	}
-})
+}
