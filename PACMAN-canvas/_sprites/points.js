70ad60c0e870bc1dfd30db4204d75f8a486de728
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-10-30 07:46:39 +0900

    Add files via upload

---
+++ PACMAN-canvas/_sprites/points.js
@@ -0,0 +1,73 @@
+const FruitCvs = canvas2D(null)
+const GhostCvs = canvas2D(null)
+
+export const Vals = /**@type {const}*/({
+	All:  [100,200,300,400,500,700,800,1e3,1600,2e3,3e3,5e3],
+	Ghost:[200,400,800,1600],
+	Fruit:[100,300,500,700,1e3,2e3,3e3,5e3],
+})
+const Data = /**@type {const}*/({
+	PathFrom0To8: [
+		[1,0,2,0,3,1,3,5,2,6,1,6,0,5,0,1],
+		[0,1,1,0,1,6,0,6,2,6],
+		[0,2,0,1,1,0,3,0,4,1,4,2,0,6,4,6],
+		[0,0,4,0,2,2,4,4,4,5,3,6,1,6,0,5],
+		[3,6,3,0,0,3,0,4,4,4],
+		[4,0,0,0,0,2,3,2,4,3,4,5,3,6,1,6,0,5],
+		[3,0,1,0,0,1,0,5,1,6,2,6,3,5,3,3,0,3],
+		[0,1,0,0,4,0,4,1,2,4,2,6],
+		[1,0,3,0,4,1,4,2,3,3,1,3,0,4,0,5,1,6,3,6,4,5,4,4,3,3,1,3,0,2,0,1],
+	],
+	DigitOffsetY: -3,
+	DigitOffsetXFrom: {
+		 100: [ -6.1, -2.3, 2.7],
+		 200: [ -7.0, -1.0, 4.0],
+		 300: [ -7.2, -1.2, 3.8],
+		 400: [ -7.1, -1.2, 3.8],
+		 500: [ -7.0, -1.0, 4.0],
+		 700: [ -7.0, -1.0, 4.0],
+		 800: [ -7.0, -1.0, 4.0],
+		1000: [ -8.0, -4.0, 1.0, 6.0],
+		1600: [ -7.4, -5.3, -.3, 4.7],
+		2000: [-10.0, -4.0, 1.0, 6.0],
+		3000: [-10.0, -4.0, 1.0, 6.0],
+		5000: [-10.0, -4.0, 1.0, 6.0],
+	}
+})
+/** @type {ReadonlySet<number>} */
+const GtsPtsSet = new Set(Vals.Ghost)
+
+/**
+ * @typedef {typeof Vals.All[number]} PtsType
+ * @param {PtsType} pts
+ */
+export function cache(pts, size=TileSize*2) {
+	const idx  = +GtsPtsSet.has(pts)
+	const ctx  = [FruitCvs, GhostCvs][idx].ctx
+	const cst  = [Color.FruitPts, Color.GhostPts][idx]
+	const[w,h] = ctx.resize(size*1.5, size).size
+	ctx.clear()
+	ctx.save()
+	ctx.translate(w/2, h/2)
+	ctx.scale(size/16, size/16)
+	ctx.strokeStyle = cst
+	ctx.lineWidth = 1.2
+	ctx.lineJoin  = ctx.lineCap = 'round'
+	Data.DigitOffsetXFrom[pts]?.forEach((x,i)=> {
+		const y = Data.DigitOffsetY
+		const d = +pts.toString()[i]
+		pts == 1600 && i == 0 ?
+		ctx.newLinePath([x,y],[x,y+6]) : //narrow 1
+		function() {
+			ctx.save()
+			ctx.translate(x,y)
+			ctx.newLinePath(.../**@type {xyList[]}*/
+				(chunk(Data.PathFrom0To8[d],2)))
+			;(d == 0 || d == 8) && ctx.closePath()
+			ctx.restore()
+		}()
+		ctx.stroke()
+	})
+	ctx.restore()
+	return /**@type {const}*/({ctx,w,h})
+}
