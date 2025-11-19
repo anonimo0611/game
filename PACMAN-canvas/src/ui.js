38fe84128c0b1d4f261f9779419a6ea13e75b684
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-11-20 07:59:52 +0900

    Add files via upload

--- PACMAN-canvas/src/ui.js
+++ PACMAN-canvas/src/ui.js
@@ -5,24 +5,24 @@ export const Menu = freeze({
 	Level: new _Menu.DorpDown('LevelMenu'),
 	Extend:new _Menu.Slide('ExtendMenu'),
 }),
-MenuIds = /**@type {(keyof Menu)[]}*/(keys(Menu))
+MenuIds = /**@type {(keyof Menu)[]}*/(keys(Menu));
 
 //---- Panels ----
 
-;/** @type {HTMLButtonElement[]} */
+/** @type {HTMLButtonElement[]} */
 (qSAll('.panelBtn')).forEach(btn=> {
 	$(btn).on('keydown pointerdown', e=> {
 		if (nonEnterKey(e))
 			return
 		$('.panel').toggle()
-		btn.classList.toggle('opened')
+		$(btn).toggleClass('opened')
 	})
 	$(dBody).on('keydown pointerdown', e=> {
 		if (!btn.offsetParent
-		 || e.target == btn
-		 || e.target.closest(btn.value))
-		 	return
+		|| e.target == btn
+		|| e.target.closest(btn.value))
+			return
 		$(btn.value).hide()
-		btn.classList.remove('opened')
+		$(btn).removeClass('opened')
 	})
-}),$('.panel').hide()
+})
