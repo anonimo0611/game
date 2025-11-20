070d2bc879a6662642e3f2ecfbdcfedd5dd016c9
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-11-20 16:00:16 +0900

    Add files via upload

--- PACMAN-canvas/src/demo/_run_timer.js
+++ PACMAN-canvas/src/demo/_run_timer.js
@@ -1,20 +1,21 @@
-// The attract demo begins after 30 seconds of inactivity
-const  WaitTime = 1e3*30 // ms
 import {Confirm} from '../../_lib/confirm.js'
 import {Ctrl}    from '../control.js'
 import {State}   from '../state.js'
+
+/** The attract demo begins after 30 seconds of inactivity */
+const WaitTime = 1e3*30 // ms
+
 export const RunTimer = function() {
-	let fcnt = 0
+	let _cnt = 0
 	function update() {
-		(!document.hasFocus()
-			|| !State.isTitle
-			|| Confirm.opened
-			|| Ctrl.activeElem
-		)? (fcnt=0)
-		 : (fcnt++)*Ticker.Interval > WaitTime
-		 	&& State.to('Attract')
+		!document.hasFocus()
+		  || !State.isTitle
+		  || Confirm.opened
+		  || Ctrl.activeElem
+		? _cnt=0
+		: _cnt++ * Ticker.Interval > WaitTime
+			&& State.to('Attract')
 	}
-	$win.on(`Title blur click focus
-		mousemove keydown resize wheel`,()=>{fcnt=0})
+	$win.on(`Title blur click focus mousemove keydown resize wheel`, ()=> _cnt=0)
 	return {update}
 }()
