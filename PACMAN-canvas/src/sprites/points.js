5ea6ddab92c4eef36cf8fb9b047e9bee2822db5d
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-11-22 19:56:54 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/points.js
+++ PACMAN-canvas/src/sprites/points.js
@@ -1,73 +1,49 @@
-const FruitCvs = canvas2D(null)
-const GhostCvs = canvas2D(null)
+import {Game}   from './_main.js'
+import {State}  from './state.js'
+import {Score}  from './score.js'
+import {GhsMgr} from './ghosts/_system.js'
+import {Fruit}  from './fruit.js'
+import * as Pts from './sprites/points.js'
 
-export const Score = /**@type {const}*/({
-	All:  [100,200,300,400,500,700,800,1e3,1600,2e3,3e3,5e3],
-	Ghost:[200,400,800,1600],
-	Fruit:[100,300,500,700,1e3,2e3,3e3,5e3],
-})
-const Data = /**@type {const}*/({
-	PathFrom0To8: [
-		[1,0,2,0,3,1,3,5,2,6,1,6,0,5,0,1],
-		[0,1,1,0,1,6,0,6,2,6],
-		[0,2,0,1,1,0,3,0,4,1,4,2,0,6,4,6],
-		[0,0,4,0,2,2,4,4,4,5,3,6,1,6,0,5],
-		[3,6,3,0,0,3,0,4,4,4],
-		[4,0,0,0,0,2,3,2,4,3,4,5,3,6,1,6,0,5],
-		[3,0,1,0,0,1,0,5,1,6,2,6,3,5,3,3,0,3],
-		[0,1,0,0,4,0,4,1,2,4,2,6],
-		[1,0,3,0,4,1,4,2,3,3,1,3,0,4,0,5,1,6,3,6,4,5,4,4,3,3,1,3,0,2,0,1],
-	],
-	DigitOffsetY: -3,
-	DigitOffsetXFrom: {
-		 100: [ -6.1, -2.3, 2.7],
-		 200: [ -7.0, -1.0, 4.0],
-		 300: [ -7.2, -1.2, 3.8],
-		 400: [ -7.1, -1.2, 3.8],
-		 500: [ -7.0, -1.0, 4.0],
-		 700: [ -7.0, -1.0, 4.0],
-		 800: [ -7.0, -1.0, 4.0],
-		1000: [ -8.0, -4.0, 1.0, 6.0],
-		1600: [ -7.4, -5.3, -.3, 4.7],
-		2000: [-10.0, -4.0, 1.0, 6.0],
-		3000: [-10.0, -4.0, 1.0, 6.0],
-		5000: [-10.0, -4.0, 1.0, 6.0],
-	}
-})
-/** @type {ReadonlySet<number>} */
-const GtsPtsSet = new Set(Score.Ghost)
+const PtsMap = /**@type {Map<any, Points>}*/(new Map)
+State.on({_Clear_Crashed:()=> PtsMap.clear()})
 
 /**
- @typedef {typeof Score.All[number]} PtsType
- @param {PtsType} pts
+ @typedef {typeof Pts.Score.All[number]} Pts
+ @typedef {{key:{score:Pts}, pos:Position, dur?:number, fn?:function}} PtsData
 */
-export function cache(pts, size=TileSize*2) {
-	const idx  = +GtsPtsSet.has(pts)
-	const ctx  = [FruitCvs, GhostCvs][idx].ctx
-	const cst  = [Color.FruitPts, Color.GhostPts][idx]
-	const[w,h] = ctx.resize(size*1.5, size).size
-	ctx.clear()
-	ctx.save()
-	ctx.translate(w/2, h/2)
-	ctx.scale(size/16, size/16)
-	ctx.strokeStyle = cst
-	ctx.lineWidth = 1.2
-	ctx.lineJoin  = ctx.lineCap = 'round'
-	Data.DigitOffsetXFrom[pts]?.forEach((x,i)=> {
-		const y = Data.DigitOffsetY
-		const d = +pts.toString()[i]
-		pts == 1600 && i == 0 ?
-		ctx.newLinePath([x,y],[x,y+6]) : //narrow 1
-		function() {
-			ctx.save()
-			ctx.translate(x,y)
-			ctx.newLinePath(.../**@type {xyList[]}*/
-				(chunk(Data.PathFrom0To8[d],2)))
-			;(d == 0 || d == 8) && ctx.closePath()
-			ctx.restore()
-		}()
-		ctx.stroke()
-	})
-	ctx.restore()
-	return /**@type {const}*/({ctx,w,h})
+export const PtsMgr = new class {
+	/** @type {(data:PtsData, fn?:function)=> void} */
+	set(data,fn)   {new Points({...data,fn})}
+	update()       {PtsMap.forEach(v=> v.update())}
+	drawFruitPts() {PtsMap.get(Fruit) ?.draw()}
+	drawGhostPts() {PtsMap.get(GhsMgr)?.draw()}
+}
+class Points {
+	/** @param {PtsData} data */
+	constructor({key,pos,dur=1e3,fn}) {
+		const spd  = Game.speed, fadeDur = 300
+		this.pos   = pos
+		this.cache = Pts.cache(key.score)
+		this.fade  = new FadeOut(fadeDur/spd, (dur-fadeDur)/spd)
+		Timer.set(dur/spd, ()=> {
+			Timer.unfreeze()
+			PtsMap.delete(key)
+			fn?.()
+		})
+		State.isPlaying && Score.add(key.score)
+		PtsMap.set(key, this)
+	}
+	update() {
+		this.fade.update()
+	}
+	draw() {
+		const sideOfst = T*1.25
+		const {pos:{x,y},cache:{ctx,w,h}}= this
+		Ctx.save()
+		Ctx.setAlpha(this.fade?.alpha)
+		Ctx.translate(clamp(x, sideOfst, BW-sideOfst), y)
+		Ctx.drawImage(ctx.canvas, -w/2,-h/2)
+		Ctx.restore()
+	}
 }
