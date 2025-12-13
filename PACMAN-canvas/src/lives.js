cc015ee08023acf10f4feeffa4e7e60fbd25ffef
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-12-14 06:01:24 +0900

    Add files via upload

--- PACMAN-canvas/src/lives.js
+++ PACMAN-canvas/src/lives.js
@@ -1,6 +1,7 @@
-import {State} from './state.js'
-import {Ctrl}  from './control.js'
-import Sprite  from './sprites/pacman.js'
+import {State}  from './state.js'
+import {Ctrl}   from './control.js'
+import {inputs} from './inputs.js'
+import Sprite   from './sprites/pacman.js'
 
 export const Lives = function() {
 	let _left = 0
@@ -17,7 +18,7 @@ export const Lives = function() {
 		const radius = T*.78, size = T*2
 		HUD.save()
 		HUD.translate(size, BH-size)
-		HUD.clearRect(0,0, size*5, size)
+		HUD.clearRect(0,0, size*(+inputs.lvsRng.max), size)
 		for (const i of range(left))
 			sprite.draw({radius,center:{x:size*i+T, y:T}})
 		HUD.restore()
@@ -25,7 +26,7 @@ export const Lives = function() {
 	function reset()  {draw(_left = Ctrl.livesMax-1)}
 	function add(n=0) {draw(_left += n)}
 
-	$('#lvsRng').on({input:onChange})
+	$(inputs.lvsRng).on({input:onChange})
 	State.on({_Intro_Ready_Restarted:onChange})
 	return {
 		append()   {add(1)},
