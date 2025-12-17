e539ef47825a513d1c88ff9b5475cd36c21de43f
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2025-12-18 03:30:08 +0900

    Add files via upload

--- PACMAN-canvas/src/maze.js
+++ PACMAN-canvas/src/maze.js
@@ -164,10 +164,9 @@ export const Maze = freeze(new class {
 			return
 		Ctx.save()
 		Ctx.beginPath()
-		for (const y of range(1,Cols)) Ctx.setLinePath([T*y, 0], [T*y, Rows*T])
-		for (const x of range(0,Rows)) Ctx.setLinePath([0, T*x], [Cols*T, T*x])
 		Ctx.strokeStyle = Colors.Grid
-		Ctx.stroke()
+		for (const y of range(1,Cols)) Ctx.strokeLine(T*y, 0, T*y, Rows*T)
+		for (const x of range(0,Rows)) Ctx.strokeLine(0, T*x, Cols*T, T*x)
 		Ctx.restore()
 	}
 }), {drawDot}= Maze
--- PACMAN-canvas/src/sprites/wall.js
+++ PACMAN-canvas/src/sprites/wall.js
@@ -1,4 +1,5 @@
 const W  = Cols
+const HT = T/2
 const OO = 2 // Outer Offset
 const LO = 3 // Line Offset
 const LW = 3 // Line Width
@@ -19,8 +20,7 @@ export const Wall = new class {
 	flashing(/**@type {function}*/fn) {
 		let count = 0
 		;(function redraw() {
-			if (++count > 8)
-				return Timer.set(500, fn)
+			if (++count > 8) return Timer.set(500, fn)
 			Wall.draw([, Colors.FlashWall][count % 2])
 			Timer.set(250, redraw)
 		})()
@@ -36,7 +36,7 @@ export const Wall = new class {
 		Bg.translate(BW/2, Maze.House.MiddleY)
 		Bg.strokeRect(-ox, -oy, ox*2, oy*2)
 		Bg.strokeRect(-ix, -iy, ix*2, iy*2)
-		Bg.clearRect (-T, -oy-LW,T*2, T/2)
+		Bg.clearRect (-T, -oy-LW, T*2, HT)
 		Bg.strokeLine(-T-LW/2, -oy, -T-LW/2, -iy)
 		Bg.strokeLine(+T+LW/2, -oy, +T+LW/2, -iy)
 		Bg.restore()
@@ -46,18 +46,17 @@ export const Wall = new class {
 	*/
 	#drawCorner({type,ci,x,y}) {
 		Bg.save()
-		Bg.translate(x+T/2, y+T/2)
+		Bg.translate(x+HT, y+HT)
 		Bg.rotate(ci*PI/2)
 		Bg.beginPath()
 		switch(type) {
-		case 0: Bg.arc(T/2,T/2, T-OO,   PI,-PI/2);break
-		case 1: Bg.arc(T/2,T/2, T/2+LO, PI,-PI/2);break
-		case 2: Bg.arc(T/2,T/2, T/2-LO, PI,-PI/2);break
-		case 3: Bg.arc(T/2,T/2, OO,     PI,-PI/2);break
-		case 4:
-			Bg.moveTo(-LO, T/2)
+		case 0: Bg.arc(HT,HT, T-OO,  PI,-PI/2);break
+		case 1: Bg.arc(HT,HT, HT+LO, PI,-PI/2);break
+		case 2: Bg.arc(HT,HT, HT-LO, PI,-PI/2);break
+		case 3: Bg.arc(HT,HT, OO,    PI,-PI/2);break
+		case 4: Bg.moveTo(-LO,  HT)
 			Bg.arcTo (-LO, -LO, T/3-LO, -LO, T/3)
-			Bg.lineTo(T/2, -LO)
+			Bg.lineTo( HT, -LO)
 			break
 		}
 		Bg.stroke()
@@ -68,28 +67,28 @@ export const Wall = new class {
 	 @param {number} i Tile index
 	*/
 	#drawTile(c, i) {
-		const [n,tx,ty]= [Number(c),i%W,i/W|0], [x,y]= [tx*T,ty*T]
-		const ci = n? n-(n>4? 5:1):'ABCD'.indexOf(c.toUpperCase())
-		const lo = (c == '#' && tx<W/2) || /[VH=]/.test(c)? -LO:LO
+		const [n,tx,ty]= [Number(c),i%W,int(i/W)], [x,y]= [tx*T,ty*T]
+		const ci = n? n-(n>4 ? 5:1) : 'ABCD'.indexOf(c.toUpperCase())
+		const lo = (c == '#' && tx<W/2) || /[VH=]/.test(c) ? -LO:LO
 
-		switch(c.toUpperCase()) {
-		case '#':
-		case 'V':Bg.strokeLine(x+T/2+lo, y, x+T/2+lo, y+T);break
-		case 'H':Bg.strokeLine(x, y+T/2+lo, x+T, y+T/2+lo);break
+		switch(c.replace('#','V').toUpperCase()) {
+		case 'V':Bg.strokeLine(x+HT+lo, y, x+HT+lo, y+T);break
+		case 'H':Bg.strokeLine(x, y+HT+lo, x+T, y+HT+lo);break
 		}
-		;[/[A-D]/,/[A-D]/,/[a-d1-4]/,/[a-d]/,/[5-8]/].forEach(
-			(r,type)=> r.test(c) && Wall.#drawCorner({type,ci,x,y}))
+
+		[/[A-D]/,/[A-D]/,/[a-d1-4]/,/[a-d]/,/[5-8]/].forEach(
+			(r,i)=> r.test(c) && Wall.#drawCorner({type:i,ci,x,y}))
 
 		Bg.save()
 		if (c == '#' || (!tx || tx == W-1) && n) {
-			Bg.translate(x+T/2+(tx<W/2 ? -T/2+OO : T/2-OO), y)
-			Bg.strokeLine(0,0, 0,T)
+			Bg.translate(x+(tx<W/2 ? OO:T-OO), y)
+			Bg.strokeLine(0,0,0,T)
 		}
 		if (/[_=]/.test(c) || Maze.isTopOrBottom(ty) && n) {
-			const oY = /[=56]/.test(c) ? -T/2+OO : T/2-OO
-			Bg.translate(x, y+T/2)
-			Bg.strokeLine(0,oY, T,oY)
-			!n && Bg.strokeLine(0,lo, T,lo)
+			const oY = /[=56]/.test(c) ? OO:T-OO
+			Bg.translate(x, y)
+			Bg.strokeLine(0, oY, T, oY)
+			!n && Bg.strokeLine(0, HT+lo, T, HT+lo)
 		}
 		Bg.restore()
 	}
