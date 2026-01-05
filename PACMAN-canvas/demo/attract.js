58e00949dca3afad547e77b6ac5b8ef3a5bf0b3c
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-01-05 23:32:04 +0900

    Add files via upload

---
+++ PACMAN-canvas/demo/attract.js
@@ -0,0 +1,136 @@
+﻿import {State}    from '../state.js'
+import {drawText} from '../message.js'
+import {Ctrl}     from '../control.js'
+import {Score}    from '../score.js'
+import {Fruit}    from '../fruit.js'
+import {drawDot}  from '../maze.js'
+import {Actor}    from '../actor.js'
+import {GhsMgr}   from '../ghosts/_system.js'
+import {Ghost}    from '../ghosts/ghost.js'
+import {PacMan}   from '../actor.js'
+import {RunTimer} from './_run_timer.js'
+
+export class Attract {
+	static #attract = /**@type {?Attract}*/(null)
+	static {State.on({Attract:this.#begin})}
+
+	static #begin() {
+		Attract.#attract = new Attract
+	}
+	static update() {
+		RunTimer.update()
+		Attract.#attract?.update()
+	}
+	static draw() {
+		Attract.#attract?.draw()
+		return State.isAttract
+	}
+	powIdx = 1
+	ghosts = /**@type {Ghost[]}*/([])
+	pacman = new PacMan
+	subAct = new EnergizerAct(this.pacman, this.ghosts)
+
+	/** @private */
+	constructor() {
+		range(GhsType.Max).forEach(i=> this.setActor(i))
+		GhsMgr.trigger('Init', this.ghosts)
+		$onNS('.Attract', {click_keydown_blur:this.quit})
+	}
+	get subActStarted() {
+		return Ticker.elapsedTime > 1e4+500
+	}
+	setActor(type=0) {
+		const
+		g = new Ghost(L, {type,tile:[Cols+6+(type*2),19]})
+		g.type == 0 && this.pacman.pos.set(g.x-(T*3.5), g.y)
+		this.ghosts.push(g)
+	}
+	update() {
+		if (this.subActStarted) {
+			this.subAct.update()
+			this.powIdx ^= +!(Ticker.count % PowDotInterval)
+		}
+	}
+	GhsEntries = /**@type {const}*/([
+		// time, col1, col2, row, txt1, txt2
+		[10, 8, 18,  6, 'OIKAKE----', '"AKABEI"'],
+		[30, 8, 19,  9, 'MACHIBUSE--','"PINKY"' ],
+		[50, 8, 18, 12, 'KIMAGURE--', '"AOSUKE"'],
+		[70, 8, 18, 15, 'OTOBOKE---', '"GUZUTA"'],
+	])
+	draw() {
+		Score.draw()
+		drawText(7, 5, null, 'CHARACTOR　/　NICKNAME')
+		const et = (Ticker.elapsedTime/100), Small ={size:T*.68}
+		this.GhsEntries.forEach(([t,col1,col2,row,txt1,txt2],i)=> {
+			et > t    && this.drawGhostOnTable(i,row)
+			et > t+ 5 && drawText(col1, row+1, GhsColors[i], txt1)
+			et > t+10 && drawText(col2, row+1, GhsColors[i], txt2)
+		})
+		if (et > 85) {
+			drawDot(Ctx, 10, 24)
+			drawDot(Ctx, 10, 26, true, !!this.powIdx)
+			drawText(12.0, 25, null, DotPts)
+			drawText(12.0, 27, null, PowPts)
+			drawText(14.3, 25, null,'PTS', Small)
+			drawText(14.3, 27, null,'PTS', Small)
+		}
+		if (et > 90) {
+			const {extendScore}= Ctrl
+			if (this.pacman.dir == L) {
+				drawDot(Ctx, 4, 19, true, !!this.powIdx)
+			}
+			if (extendScore > 0) {
+				const text = `BONUS　PACMAN　FOR　${extendScore}`
+				drawText( 2.0, 30, Colors.Orange, text)
+				drawText(24.3, 30, Colors.Orange,'PTS', Small)
+			}
+		}
+		if (this.subActStarted)
+			Actor.draw(this.pacman)
+		Fruit.drawLevelCounter()
+	}
+	drawGhostOnTable(type=0, row=0) {
+		this.ghosts[0].sprite.draw({type,orient:R,x:(T*5),y:(T*row)})
+	}
+	quit() {
+		Attract.#attract = null
+		State.toTitle()
+		$off('.Attract')
+	}
+}
+
+class EnergizerAct {
+	pacVelX = -BW/180
+	ghsVelX = -BW/169
+	constructor(
+	 /**@type {PacMan} */pacman,
+	 /**@type {Ghost[]}*/ghosts
+	) {
+		this.pacman = pacman
+		this.ghosts = ghosts
+	}
+	update() {
+		if (Timer.frozen) return
+		this.pacman.sprite.update()
+		this.pacman.x += this.pacVelX * Ticker.delta
+		if (this.pacman.dir == L
+		 && this.pacman.x <= T*4
+		) {
+			this.pacVelX *= -1
+			this.ghsVelX /= -2
+			this.pacman.dir = R
+			GhsMgr.setFrightMode()
+		}
+		this.#updateGhosts()
+	}
+	#updateGhosts() {
+		this.ghosts.forEach(g=> {
+			g.x += this.ghsVelX * Ticker.delta
+			g.collidesWith(this.pacman.pos, T/4, this.#end)
+		})
+	}
+	#end() {
+		GhsMgr.caughtAll && State.toAttract()
+	}
+}
