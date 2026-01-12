6ecceb42dfa114a02a5addec38e5790f16858d93
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-01-13 05:16:27 +0900

    Add files via upload

--- _lib/navi.js
+++ _lib/navi.js
@@ -1,5 +1,4 @@
-﻿const ClickEv = window.ontouchstart? 'touchend' : 'click'
-const Toggle  = document.createElement('button')
+﻿const Toggle  = document.createElement('button')
 const Header  = dqs('header')
 const NavRoot = dqs('header > nav')
 const NavToc  = dqs('header > nav > .toc')
@@ -10,19 +9,19 @@ Header && $on('resize', ()=> {
 		Header.style.height = ''
 		return;
 	}
-	const offsetTop = Header.parentNode.offsetTop;
+	const offsetTop = document.body.offsetTop;
 	Header.style.height = `calc(${innerHeight-offsetTop}px - 3em)`
 }).trigger('resize')
 
 // ナビゲーションのトグルボタン
 !function() {
 	if (!Header) return
-	const prif = 'ナビゲーションを'
-	const suff = ['隠す','表示']
-	Toggle.id     = 'nav-toggle'
-	Toggle.title  = prif+suff[1]
+	const prif   = 'ナビゲーションを'
+	const suff   = ['隠す','表示']
+	Toggle.id    = 'nav-toggle'
+	Toggle.title = prif+suff[1]
 	Toggle.textContent = Toggle.title
-	Toggle.addEventListener(ClickEv, ()=> {
+	Toggle.addEventListener('click', ()=> {
 		const data   = NavRoot.dataset.hidden
 		const hidden = !data || data == 'true'
 		Toggle.textContent = Toggle.title
@@ -31,57 +30,102 @@ Header && $on('resize', ()=> {
 		NavRoot.dataset.hidden = !hidden
 	})
 	Header.prepend(Toggle)
-	$on(ClickEv, e=> {
+	$on('click', e=> {
 		(e.target != Toggle) && !e.target.closest('header > nav')
 			&& (NavRoot.dataset.hidden = true)
 			&& (Toggle.title = prif+suff[1])
 	})
 }()
 
-// スクロール位置の見出しに対応する目次項目の強調
-!function() {
-	if (!Header) return
-	const elements = [...dqsAll('section')].reverse().flatMap(section=> {
-		if (!section.id) return []
-		const  anchor = Header.querySelector(`[href="#${section.id}"]`) || null
-		return anchor? {section,anchor} : []
+// スクロールに応じて目次のアンカーを強調
+;(()=> {
+	const dRoot  = document.documentElement
+	const secs   = document.querySelectorAll('#main-content,section[id]')
+	const navMap =  /**@type {Map<HTMLElement,HTMLAnchorElement>}*/(new Map)
+	const emToPx = (/**@type {number}*/em)=> em * parseFloat(getComputedStyle(dRoot).fontSize)
+
+	const navEntry = /** @type {[HTMLElement,HTMLAnchorElement][]}*/([])
+	secs.forEach(sec=> {
+		const target = sec.querySelector('h1,h2,h3') || sec
+		const a = Header.querySelector(`[href="#${sec.id}"]`)
+		a && navEntry.push([target,a])
+		a && navMap.set(target,a)
 	})
-	if (!elements.length) return
-	let   currentA = null
-	const isNearly = elm=>
-		(elm.section.offsetTop <= dRoot.scrollTop)
-	$on('scroll resize', ()=> {
-		const elm = elements.find(isNearly)
-		if (!elm || elm.anchor == currentA)
+
+	/** @type {?HTMLAnchorElement} */
+	let currentA = dqs('.to-top > a[href]')
+	    currentA?.classList.add('current')
+
+	const updateHighlight = anchor=> {
+		if (!anchor || anchor === currentA) return
+		currentA?.classList.remove('current')
+		anchor.classList.add('current')
+		currentA = anchor
+		anchor.scrollIntoView({behavior:'auto',block:'nearest'})
+	}
+
+	const observer = new IntersectionObserver(entries=> {
+		const
+		visibleEntry = entries.find(e=> e.isIntersecting)
+		visibleEntry && updateHighlight(navMap.get(visibleEntry.target))
+	}, {
+		rootMargin: `-${emToPx(1)}px 0px -95% 0px`,
+		threshold: [0, 1.0]
+	})
+
+	navMap.forEach((_, target)=> observer.observe(target))
+	Header.addEventListener('click', e=> {
+		/** @type {HTMLAnchorElement} */
+		const a = e.target.closest('a')
+		a?.hash && updateHighlight(a)
+	})
+	let scrollTimeout = 0
+	function onScroll() {
+		clearTimeout(scrollTimeout)
+		scrollTimeout = setTimeout(syncNavigation, 100)
+	}
+	function syncNavigation() {
+		// 最下部判定
+		if ((window.innerHeight+window.scrollY) >= dRoot.scrollHeight-2) {
+			updateHighlight(navEntry[navEntry.length-1][1])
 			return
-		if (Header.offsetHeight < Header.scrollHeight)
-			Header.scroll(0, elm.anchor.parentNode.offsetTop)
-		elm.anchor.classList.add('current')
-		currentA && currentA.classList.remove('current')
-		currentA = elm.anchor
-	})
-	.trigger('resize')
-}()
+		}
+		// 現在位置の再計算
+		const topMargin = emToPx(1)
+		const currentTarget = navEntry.find(([target])=> {
+			const  rect = target.getBoundingClientRect()
+			return rect.top <= topMargin+10 && rect.bottom >= topMargin
+		})
+		currentTarget
+			? updateHighlight(navMap.get(currentTarget))
+			: window.scrollY === 0 && updateHighlight(navEntry[0][1])
+	}
+	addEventListener('scroll', onScroll, {passive:true})
+})()
 
 // 見出し位置へスクロール
-$('header').on(ClickEv, '[href^="#"]', e=> {
-	const h = $('#'+e.target.href.split('#')[1]).get(0)
+$('header').on('click', 'a[href^="#"]', e=> {
+	const a = /**@type {?HTMLAnchorElement}*/(e.target)
+	const h = $(a.hash).get(0)
 	if (h) {
 		e.preventDefault()
+		history.replaceState(null,'',a.hash);
 		h.scrollIntoView()
 	}
 })
+
 // フォーカスされたらナビ表示
 $('header').on('focus', 'a[href]', ()=> {
 	NavRoot.dataset.hidden = 'false'
 })
 
 // 見出しへのリンクをコピー
-$('[href^="#"].fragment').on(ClickEv, e=> {
+$('a[href^="#"].fragment').on('click', e=> {
+	const a = /**@type {?HTMLAnchorElement}*/(e.target)
 	if (!document.execCommand) return
 	e.preventDefault()
 	e.target.scrollIntoView()
-	const href  = e.target.href
+	const href  = a.href
 	const style = 'style="position:absolute;left:-1000px;top:-1000px"'
 	const input = $(`<input type="text" value="${href}" ${style}>`).get(0)
 	document.body.appendChild(input)
