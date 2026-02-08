da081e4e161042c55707fa732ecc109f10a60c88
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-02-08 19:22:14 +0900

    Add files via upload

--- PACMAN-canvas/src/demo/_demo.js
+++ PACMAN-canvas/src/demo/_demo.js
@@ -4,22 +4,31 @@ import {State}     from '../state.js'
 import {Attract}   from './attract.js'
 import {CoffBreak} from './coffee_break.js'
 
+const Events = 'scroll_pointerdown_mousemove_keydown_resize_wheel'
+
 /** Attract Mode will begin after a period of inactivity. */
-const WaiteTime = 1e3*30 // 30secs
-const RunTimer  = new class {
+const RunTimer = new class {
 	#cnt = 0
-	constructor() {
-        $win.on(`Title blur focus scroll pointerdown
-			mousemove keydown resize wheel`, ()=> this.#cnt = 0)
-    }
-    update() {
-        ;(!document.hasFocus()
-		 || Confirm.opened
-		 || Ctrl.activeElem
-		)? (this.#cnt = 0)
-         : (this.#cnt+=Ticker.Interval) > WaiteTime
-		 	&& State.toAttract()
-    }
+	constructor() {State.onChange(()=> this.sync())}
+	reset()  {this.#cnt = 0}
+	pause()  {Ticker.pause(true) && this.reset()}
+	resume() {Ticker.pause(false)}
+	sync() {
+		this.reset()
+		!State.isTitle?
+			$off('.RunTimer'):
+			$onNS('RunTimer', {
+				blur:    ()=> this.pause(),
+				focus:   ()=> this.resume(),
+				[Events]:()=> this.reset(),
+			})
+	}
+	update() {
+		(!document.hasFocus() || Confirm.opened || Ctrl.activeElem)
+			? this.reset()
+		 	: (this.#cnt+=Ticker.Interval) > 1e3*30 // 30secs
+				&& State.toAttract()
+	}
 }
 
 /** @typedef {import('../state.js').StateType} StateType */
