4cdea80f8e1e67cf7396293df919ca7e8ed48a54
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-02-13 05:12:13 +0900

    Add files via upload

--- PACMAN-canvas/src/actor_fader.js
+++ PACMAN-canvas/src/actor_fader.js
@@ -2,19 +2,22 @@ import {State} from './state.js';
 export class SpawnFader {
 	constructor(dur=500, isFadeOut=false) {
 		/** @private @type {Fade} */
-		this.fader = Fade[isFadeOut?'out':'in'](dur)
+		this.fader = Fade.set(dur,{isFadeOut})
+	}
+	set(dur=500, isFadeOut=false) {
+		this.fader = Fade.set(dur,{isFadeOut})
 	}
 	alpha(max=1) {
 		if (State.isDemoMode) return 1
 		const {fader}= this
-		return this.fader.isIn
+		return fader.isIn
 			? State.isReady? fader.alpha:max
 			: fader.alpha
 	}
 	update(max=1) {
 		const {fader}= this
 		if (!fader.running) return
-		this.fader.isIn
+		fader.isIn
 			? State.isReady && fader.update(max)
 			: fader.update(max)
 	}
