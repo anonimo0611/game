7cff889bd5537b8cba00518a4aa692e3f5de0a0f
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-06 06:14:42 +0900

    Add files via upload

--- PACMAN-canvas/src/ghosts/show_path.js
+++ PACMAN-canvas/src/ghosts/show_path.js
@@ -13,7 +13,7 @@ export class PathMgr {
 	/** @param {readonly Ghost[]} ghosts */
 	static draw(ghosts) {
 		if (State.isInGame && Ctrl.showPaths)
-			ghosts.toReversed().forEach(g=> g.pathMgr.draw())
+			ghosts.toReversed().forEach(g=> g.pathMgr.#draw())
 	}
 	#path = /**@type {PathNode[]}*/([])
 	/** @private @readonly */g
@@ -21,7 +21,6 @@ export class PathMgr {
 	constructor(ghost) {this.g = ghost}
 	get isAka() {return this.g.type == GhsType.Akabei}
 	get isEsc() {return this.g.state.isEscaping}
-	get size()  {return this.#path.length}
 	get begin() {return this.#path[0]}
 	get end()   {return this.#path.at(-1) ?? this.begin}
 	get enabled() {
@@ -30,7 +29,7 @@ export class PathMgr {
 		if (!between(g.center.x, T, BW-T)) return false
 		return g.isChasing || g.isScattering || this.isEsc
 	}
-	draw() {
+	#draw() {
 		if (!this.enabled || !this.#path.length) return
 		const {type,dir,center}= this.g, lw = T/4
 		const st   = this.begin.tile.clone.add(.5).mul(T)
@@ -53,16 +52,13 @@ export class PathMgr {
 		Fg.beginPath()
 		Fg.moveTo(...this.g.center.vals)
 		Fg.lineTo(...pos.vals)
-		this.#path.forEach((node, i) => {
+		this.#path.forEach(node=> {
 			let next = node.tile.clone.add(.5).mul(T)
 			this.#setEndTarget(node, next, dist)
 			if (abs(next.x - pos.x) > T*2) {
-				const isRightExit = next.x < pos.x
-				const margin = T/2
-				const exitX  = isRightExit? BW + margin : -margin
-				const enterX = isRightExit? -margin : BW + margin
-				Fg.lineTo(exitX,  pos.y)
-				Fg.moveTo(enterX, next.y)
+				const isR = next.x < pos.x
+				Fg.lineTo(isR? BW+T/2 : -T/2,  pos.y)
+				Fg.moveTo(isR? -T/2 : BW+T/2, next.y)
 			}
 			Fg.lineTo(...next.vals)
 			pos = next
@@ -81,28 +77,24 @@ export class PathMgr {
 	/** @param {PathNode} node */
 	#setEndTarget({stopped,tile,dir}, curr=Vec2.Zero, dist=0) {
 		if (tile != this.end.tile) return
-		stopped
-			&& tile.eq(p.tilePos)
-			&& between(p.center.x, T, BW-T)
-				? curr.set(p.center)
-				: curr.add(Vec2[dir].mul(dist))
+		const {tilePos:t,center:{x}}= p
+		stopped && tile.eq(t) && between(x,T,BW-T)
+			? curr.set(p.center)
+			: curr.add(Vec2[dir].mul(dist))
 	}
 	setPredictedPath() {
-		const {g,isAka,isEsc}=this, path=[]
+		const {g,g:{tilePos:t}}=this, path=[]
 		let dir  = g.dir
-		let tile = g.passedTileCenter
-			? g.getAdjTile(dir, g.tilePos)
-			: g.tilePos
+		let tile = g.passedTileCenter? g.getAdjTile(dir,t) : t
 		path.push({tile,dir,stopped:false})
 		for (let _ of range(Steps-1)) {
 			dir  = g.getNextDir(dir,tile)
 			tile = g.getAdjTile(dir,tile)
 			const stopped =
-				isAka && tile.eq(p.tilePos) ||
-				isEsc && tile.eq(g.targetTile)
+				this.isAka && tile.eq(p.tilePos) ||
+				this.isEsc && tile.eq(g.targetTile)
 			path.push({tile,dir,stopped})
 			if (stopped) break
-		}
-		this.#path = path
+		} this.#path = path
 	}
 }
