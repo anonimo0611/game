b03dd62787bd325d91e138a4e18c216426250235
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-19 04:34:40 +0900

    Add files via upload

--- PACMAN-canvas/_lib/_state.d.ts
+++ PACMAN-canvas/_lib/_state.d.ts
@@ -3,5 +3,5 @@ namespace StateDef {
 	type Was <S extends string> = {[K in S as `was${K}`]:boolean}
 	type Set <S extends string> = {[K in S as `set${K}`]:(opt?:Opts<S>)=> void}
 	type Is  <S extends string> = {readonly [K in S as `is${K}`]:boolean}
-	type Props<Owner,State extends string> = Is<State> & Was<State> & Set<State>
+	type Props<State extends string> = Is<State> & Was<State> & Set<State>
 }
