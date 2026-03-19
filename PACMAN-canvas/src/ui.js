b15ea321965c00c59ca179b75bafdaece60a1bf0
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-20 05:36:02 +0900

    Add files via upload

--- PACMAN-canvas/src/ui.js
+++ PACMAN-canvas/src/ui.js
@@ -16,7 +16,7 @@ const buttonIds = /**@type {const}*/
 	(['clear','reset','start','demo','coff1','coff2','coff3'])
 
 export const btns =
-	/**@type {{[K in typeof buttonIds[number]]:HTMLButtonElement}}*/
+	/**@type {{[K in buttonIds[number]]:HTMLButtonElement}}*/
 	(fromEntries(buttonIds.map(id=>[id,requireElem(id+'Btn')])))
 
 //---- Custom menus ----
