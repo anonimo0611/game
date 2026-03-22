98efd8dd13230d060a66a671efd99c6efbbc4176
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-23 02:20:03 +0900

    Add files via upload

--- PACMAN-canvas/_sprites/_constants.js
+++ PACMAN-canvas/_sprites/_constants.js
@@ -13,7 +13,7 @@ export const BrightRng = /**@type {HTMLInputElement} */(byId('brightRng'))
 export const ResetBtn  = /**@type {HTMLButtonElement}*/(byId('resetBtn'))
 
 export let  [T,S,GAP] = [0,0,0]
-export const GridSize = Vec2.new(10,11).freeze()
+export const GridSize = new Vec2(10,11).freeze()
 export const ghsSprPv = new GhsSprite(Pv)
 export const ghsSprGr = new GhsSprite(Fg)
 
