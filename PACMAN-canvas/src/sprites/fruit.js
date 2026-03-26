74cad4815a20efe3cb9ebe6e7d8c0527781d4f4b
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-27 03:07:33 +0900

    Add files via upload

---
+++ PACMAN-canvas/src/sprites/fruit.js
@@ -0,0 +1,99 @@
+import {Sound}  from '../_snd/sound.js'
+import {Rect}   from '../_lib/rect.js'
+import {Game}   from './_main.js'
+import {Level}  from './_main.js'
+import {State}  from './state.js'
+import {Maze}   from './maze.js'
+import {PtsMgr} from './points.js'
+import * as Spr from './sprites/fruits.js'
+import {player,onPlayerDotEaten} from './player/player.js'
+
+const LevelsCols = 7
+const AppearDots = new Set([70,170])
+const TargetPos  = new Vec2(BW/2, T*18.5).freeze()
+const LevelsRect = new Rect(T*12, BH-T*2, T*2*LevelsCols, T*2).freeze()
+const FruitTable = /**@type {const}*/([0,1,2,2,3,3,4,4,5,5,6,6,7])
+const PointTable = /**@type {const}*/([100,300,500,700,1e3,2e3,3e3,5e3])
+
+const FadeDur = 300
+let  _showTgt = true
+let  _fadeOut = /**@type {?Fade}*/(null)
+
+export const Fruit = new class {
+	static {$(this.setup)}
+	static setup() {
+		Level.on({change:Fruit.#setImages})
+		State.on({_Ready:Fruit.#resetTarget})
+		onPlayerDotEaten(Fruit.#onDotEaten)
+	}
+	get currentType() {
+		return Fruit.#getType(Game.level-1)
+	}
+	get points() {
+		return PointTable[Fruit.currentType]
+	}
+	get showTarget() {
+		return (State.isTitle || State.isInGame) && _showTgt
+	}
+	get intersectsWithPlayer() {
+		return circleCollision(player.center, TargetPos, T/2)
+	}
+	#getType(/**@type {number}*/i) {
+		if (i < 0) throw RangeError('Must be zero or greater.')
+		return FruitTable.at( min(i, FruitTable.length-1) ) ?? 0
+	}
+	#setHideTimer() {
+		// Disappearing is between 9 and 10 seconds
+		const delay = randInt(9e3, 1e4-FadeDur)/Game.speed
+		Timer.set(delay, Fruit.#setFadeOut, {key:Fruit})
+	}
+	#resetTarget() {
+		_fadeOut = null
+		_showTgt = State.isTitle
+	}
+	#setFadeOut() {
+		_fadeOut = Fade.out(FadeDur/Game.speed)
+	}
+	#onDotEaten() {
+		if (AppearDots.has(Maze.MaxDot - Maze.dotsLeft)) {
+			_showTgt = true
+			Fruit.#setHideTimer()
+		}
+	}
+	#checkIntersects() {
+		if (Fruit.showTarget && Fruit.intersectsWithPlayer) {
+			Fruit.#resetTarget()
+			Timer.cancel(Fruit) && Sound.playEatenSE()
+			PtsMgr.set({key:Fruit, dur:2e3, pos:TargetPos})
+		}
+	}
+	update() {
+		_fadeOut?.update() == false
+			? Fruit.#resetTarget()
+			: Fruit.#checkIntersects()
+	}
+	drawTarget() {
+		if (Fruit.showTarget && !Ticker.paused) {
+			Spr.Cache.draw(Fg, TargetPos, _fadeOut?.alpha)
+		}
+		PtsMgr.drawFruitPts()
+	}
+	drawLevelCounter() {
+		const [x,y,w,h] = LevelsRect.vals
+		Fg.drawImage(HUD.canvas, x,y,w,h, x,y,w,h)
+	}
+	#setLevelCounter() {
+		const [x,y,w,h]  = LevelsRect.vals
+		const startLevel = max(Game.level-LevelsCols, 0)
+		HUD.save()
+		HUD.clearRect(x,y,w,h)
+		HUD.translate(x,y)
+		for (const i of range(startLevel, Game.level))
+			Spr.draw(HUD, Fruit.#getType(i), w-T-T*2*(i-startLevel))
+		HUD.restore()
+	}
+	#setImages() {
+		Spr.Cache.update(Fruit.currentType)
+		Fruit.#setLevelCounter()
+	}
+}
