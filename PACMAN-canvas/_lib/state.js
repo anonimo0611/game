c6883946ad813cf6f20db93d29144f91c8f354d6
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-28 13:38:16 +0900

    Add files via upload

--- PACMAN-canvas/_lib/state.js
+++ PACMAN-canvas/_lib/state.js
@@ -1,6 +1,6 @@
 /**
- @template {object} Owner
  @template {string} S
+ @template {object} Owner
 */
 export default class StateBase {
 	#owner
@@ -50,7 +50,7 @@ export default class StateBase {
 	/** @param {{[key in S]?:JQTriggerHandler}} o */
 	on(o) {
 		for (const [state,fn] of entries(o))
-			$(this.#owner).on(underscoreToSp(state, String(this.default)), fn)
+			$(this.#owner).on(underscoreToSp(state,String(this.default)), fn)
 		return this
 	}
 
@@ -63,10 +63,19 @@ export default class StateBase {
 	}
 
 	/**
+	 @protected
+	 @param {S} state
+	 @param {JQData} [data]
+	*/
+	callback(state, data) {
+		$(this.owner).trigger(state, data)
+	}
+
+	/**
 	 @param {S} state
 	 @param {StateDef.Opts<S>} opts
 	*/
-	set(state, {data,delay=-1,fn}={}) {
+	set(state, {data,delay=-1,fn=this.callback}={}) {
 		if (delay >= 0) {
 			Timer.set(delay, ()=> this.set(state,{delay:-1,data,fn}))
 			return this
@@ -75,7 +84,7 @@ export default class StateBase {
 		this.#last = this.current
 		this.#curr = state
 		this.#eventBus.trigger('change')
-		fn?.(state,data)
+		fn?.call(this,state,data)
 		return this
 	}
 }
