3b4cf292ced034ab085b6a4bb8c6b363f284e033
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-28 15:47:57 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/points.js
+++ PACMAN-canvas/src/sprites/points.js
@@ -3,7 +3,7 @@ const ghostCtx = canvas2D(null).ctx
 const ctxList  = freeze([fruitCtx,ghostCtx])
 const palette  = freeze(['#FAF','#3CF'])
 
-const NarrowOnePath = newPath2D('M0,0 L0,6')
+const NarrowOnePath = path2D('M0,0 L0,6')
 const DigitPath0to8 = freeze([
 	'M1,0 L2,0 L3,1 L3,5 L2,6 L1,6 L0,5 L0,1 Z',
 	'M0,1 L1,0 L1,6 L0,6 L2,6',
@@ -14,8 +14,8 @@ const DigitPath0to8 = freeze([
 	'M3,0 L1,0 L0,1 L0,5 L1,6 L2,6 L3,5 L3,3 L0,3',
 	'M0,1 L0,0 L4,0 L4,1 L2,4 L2,6',
 	`M1,0 L3,0 L4,1 L4,2 L3,3 L1,3 L0,4 L0,5
-	 L1,6 L3,6 L4,5 L4,4 L3,3 L1,3 L0,2 L0,1 Z`].map(newPath2D))
-
+	 L1,6 L3,6 L4,5 L4,4 L3,3 L1,3 L0,2 L0,1 Z`].map(path2D)
+)
 const KerningMap = /**@type {const}*/({
 	 100: [ -5.9, -2.0, 3.0],
 	 200: [ -7.0, -1.0, 4.0],
@@ -28,7 +28,8 @@ const KerningMap = /**@type {const}*/({
 	1600: [ -7.4, -5.3, -.3, 4.7],
 	2000: [-10.0, -4.0, 1.0, 6.0],
 	3000: [-10.0, -4.0, 1.0, 6.0],
-	5000: [-10.0, -4.0, 1.0, 6.0]})
+	5000: [-10.0, -4.0, 1.0, 6.0],
+})
 
 /**
  @param {0|1} type 0=Fruits, 1=Ghosts
