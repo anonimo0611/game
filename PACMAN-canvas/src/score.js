c191b25ba8d90ee3b7264a72482870e42952ffbb
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-29 17:23:16 +0900

    Add files via upload

--- PACMAN-canvas/src/score.js
+++ PACMAN-canvas/src/score.js
@@ -5,7 +5,7 @@ import {drawText} from './message.js'
 import {Ctrl}     from './control.js'
 import {Lives}    from './lives.js'
 
-let [score,hiSco,savedScore,savedHiSco]= [0,0,0,0]
+let [_score,_hiSco,savedScore,savedHiSco]= [0,0,0,0]
 
 export const Score = new class {
 	/** @readonly */
@@ -20,24 +20,24 @@ export const Score = new class {
 		})
 	}
 	reset() {
-		score = 0
-		hiSco = localStorage[Score.HiScoreKey]|0
+		_score = 0
+		_hiSco = localStorage[Score.HiScoreKey]|0
 	}
 	#restore() {
 		if (Game.started) {
-			score = savedScore
-			hiSco = savedHiSco
+			_score = savedScore
+			_hiSco = savedHiSco
 		}
 	}
 	#onIntro() {
-		savedScore = score
-		savedHiSco = hiSco
-		score = 0
+		savedScore = _score
+		savedHiSco = _hiSco
+		_score = 0
 	}
 	#onGameOver() {
-		const hiSco = localStorage[Score.HiScoreKey]|0
-		if (!Ctrl.isPractice && hiSco > hiSco)
-			localStorage[Score.HiScoreKey] = hiSco
+		const hi = localStorage[Score.HiScoreKey]|0
+		if (!Ctrl.isPractice && _hiSco > hi)
+			localStorage[Score.HiScoreKey] = _hiSco
 	}
 	get #showUP() {
 		return !State.isInGame || Ticker.paused
@@ -48,20 +48,20 @@ export const Score = new class {
 	}
 	draw() {
 		drawText(2,0, this.#fgColorUP, this.#showUP? '1UP':'')
-		drawText(6,0, null, score || '00')
+		drawText(6,0, null, _score || '00')
 		Ctrl.isPractice
 			? drawText(14,0, null, 'PRACTICE')
-			: drawText(14,0, null, `HIGH　${hiSco || '00'}`)
+			: drawText(14,0, null, `HIGH　${_hiSco || '00'}`)
 	}
 	add(points=0) {
-		const total = score + points
-		if (!Ctrl.isPractice && total > hiSco) {
-			hiSco = total
+		const total = _score + points
+		if (!Ctrl.isPractice && total > _hiSco) {
+			_hiSco = total
 		}
-		if (between(Ctrl.extendScore, score+1, total)) {
+		if (between(Ctrl.extendScore, _score+1, total)) {
 			Lives.append()
 			Sound.playBellSE()
 		}
-		score = total
+		_score = total
 	}
 }
