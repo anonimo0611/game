339bfcca83085da330d2ef859e96674f2fbb69db
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-03-31 13:18:53 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/ghost_cb.js
+++ PACMAN-canvas/src/sprites/ghost_cb.js
@@ -125,7 +125,7 @@ class Snag {
 	constructor(ctx) {this.ctx = ctx}
 	CaughtX   = BW/2 + T/2
 	AkaMinX   = this.CaughtX - T
-	stakeSize = Vec2.new(T*.18, T*.70).freeze()
+	stakeSize = new ReadonlyPos(T*.18, T*.70)
 	stakePos  = freeze({
 		x: BW/2 + T*2 - this.stakeSize.x/2,
 		y: BH/2 + T*1 - this.stakeSize.y - T*.1
