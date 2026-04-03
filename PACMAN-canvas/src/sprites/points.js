2e049b08dd1499764096439b125119def0f693a2
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-04-03 17:43:18 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/points.js
+++ PACMAN-canvas/src/sprites/points.js
@@ -1,21 +1,21 @@
-const ctxList = freeze([
+const
+ctxFruit_Ghost = freeze([
 	canvas2D(null).ctx,
-	canvas2D(null).ctx,
-])
-const NarrowOnePath = path2D('M0,0 L0,6')
-const DigitPath0to8 = freeze([
-	'M1,0 L2,0 L3,1 L3,5 L2,6 L1,6 L0,5 L0,1 Z',
-	'M0,1 L1,0 L1,6 L0,6 L2,6',
-	'M0,2 L0,1 L1,0 L3,0 L4,1 L4,2 L0,6 L4,6',
-	'M0,0 L4,0 L2,2 L4,4 L4,5 L3,6 L1,6 L0,5',
-	'M3,6 L3,0 L0,3 L0,4 L4,4',
-	'M4,0 L0,0 L0,2 L3,2 L4,3 L4,5 L3,6 L1,6 L0,5',
-	'M3,0 L1,0 L0,1 L0,5 L1,6 L2,6 L3,5 L3,3 L0,3',
-	'M0,1 L0,0 L4,0 L4,1 L2,4 L2,6',
-	`M1,0 L3,0 L4,1 L4,2 L3,3 L1,3 L0,4 L0,5
-	 L1,6 L3,6 L4,5 L4,4 L3,3 L1,3 L0,2 L0,1 Z`].map(path2D)
-)
-const KerningMap = /**@type {const}*/({
+	canvas2D(null).ctx
+]),
+NarrowOnePath = path2D('M0,0 L0,6'),
+DigitPath0to8 = /**@type {NumericDict<Path2D>}*/({
+	0: path2D('M1,0 L2,0 L3,1 L3,5 L2,6 L1,6 L0,5 L0,1 Z'),
+	1: path2D('M0,1 L1,0 L1,6 L0,6 L2,6'),
+	2: path2D('M0,2 L0,1 L1,0 L3,0 L4,1 L4,2 L0,6 L4,6'),
+	3: path2D('M0,0 L4,0 L2,2 L4,4 L4,5 L3,6 L1,6 L0,5'),
+	4: path2D('M3,6 L3,0 L0,3 L0,4 L4,4'),
+	5: path2D('M4,0 L0,0 L0,2 L3,2 L4,3 L4,5 L3,6 L1,6 L0,5'),
+	6: path2D('M3,0 L1,0 L0,1 L0,5 L1,6 L2,6 L3,5 L3,3 L0,3'),
+	7: path2D('M0,1 L0,0 L4,0 L4,1 L2,4 L2,6'),
+	8: path2D(`M1,0 L3,0 L4,1 L4,2 L3,3 L1,3 L0,4 L0,5 L1,6 L3,6 L4,5 L4,4 L3,3 L1,3 L0,2 L0,1 Z`)
+}),
+KerningMap = /**@type {const}*/({
 	 100: [ -5.9, -2.0, 3.0],
 	 200: [ -7.0, -1.0, 4.0],
 	 300: [ -7.0, -1.0, 4.0],
@@ -35,7 +35,7 @@ const KerningMap = /**@type {const}*/({
  @param {PtsValue} pts
 */
 export function cache(type, pts, size=T*2) {
-	const ctx = ctxList[type]
+	const ctx = ctxFruit_Ghost[type]
 	const{w,h}= ctx.resize(size*1.5, size).size
 	ctx.save()
 	ctx.translate(w/2, h/2)
