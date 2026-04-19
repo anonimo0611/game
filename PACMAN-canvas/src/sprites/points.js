6ab63dc575a8689a6c352c8c83a5d2056c77af4b
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-04-20 04:24:26 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/points.js
+++ PACMAN-canvas/src/sprites/points.js
@@ -14,7 +14,7 @@ DigitPath0to8 = /**@type {NumericDict<Path2D>}*/({
 	5: path2D('M4,0 L0,0 L0,2 L3,2 L4,3 L4,5 L3,6 L1,6 L0,5'),
 	6: path2D('M3,0 L1,0 L0,1 L0,5 L1,6 L2,6 L3,5 L3,3 L0,3'),
 	7: path2D('M0,1 L0,0 L4,0 L4,1 L2,4 L2,6'),
-	8: path2D(`M1,0 L3,0 L4,1 L4,2 L3,3 L1,3 L0,4 L0,5 L1,6 L3,6 L4,5 L4,4 L3,3 L1,3 L0,2 L0,1 Z`)
+	8: path2D('M1,0 L3,0 L4,1 L4,2 L3,3 L1,3 L0,4 L0,5 L1,6 L3,6 L4,5 L4,4 L3,3 L1,3 L0,2 L0,1 Z')
 }),
 KerningMap = /**@type {const}*/({
 	 100: [ -5.9, -2.0, 3.0],
