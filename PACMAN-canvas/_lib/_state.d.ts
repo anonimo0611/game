c27684974e34f5bbf3063016901cb6afd3519a67
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-04-29 09:49:44 +0900

    Add files via upload

--- PACMAN-canvas/_lib/_state.d.ts
+++ PACMAN-canvas/_lib/_state.d.ts
@@ -1,7 +1,8 @@
+
 namespace StateDef {
 	type Opts<S extends string> = {delay?:number, data?:JQData, cb?:(state:S, data?:JQData)=> void}
-	type Was <S extends string> = {[K in S as `was${K}`]:boolean}
-	type Set <S extends string> = {[K in S as `set${K}`]:(opt?:Opts<S>)=> void}
-	type Is  <S extends string> = {readonly [K in S as `is${K}`]:boolean}
-	type Props<State extends string> = Is<State> & Was<State> & Set<State>
+	type Fluent<State extends object,S extends string> = State
+		& {readonly [K in S as `is${K}`]:boolean}
+		& {readonly [K in S as `was${K}`]:boolean}
+		& {readonly [K in S as `set${K}`]:(opt?:Opts<S>)=> void}
 }
