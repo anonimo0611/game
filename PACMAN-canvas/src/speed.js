f1372f1e611499c86972205c736e7b3834c957b1
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-05-06 00:53:50 +0900

    Add files via upload

--- PACMAN-canvas/src/speed.js
+++ PACMAN-canvas/src/speed.js
@@ -4,19 +4,19 @@ const BASE_G = BASE_P * 1.07
 import {Game} from './_main.js'
 export default /**@type {const}*/({
 	StepPerLevel: 0.01,
-	Pacman: freeze({
+	Pacman: {
 		Base:      BASE_P,
 		Eating:    BASE_P * 0.86,
 		Energized: BASE_P * 1.10,
 		EneEating: BASE_P * 0.95, // Energized+Eating
 		get levelFactor() {return (Game.level < 13 ? 1 : 0.98)},
-	}),
-	Ghost: freeze({
+	},
+	Ghost: {
 		Base:     BASE_G,
 		Idle:     BASE_G * 0.50,
 		GoOut:    BASE_G * 0.50,
 		Fright:   BASE_G * 0.60,
 		InTunnel: BASE_G * 0.60,
 		Escape:   BASE_G * 1.40,
-	}),
+	},
 })
