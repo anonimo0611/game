8a521d5ffd14f09887a726e748d0cf1dc6f0373d
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-05-17 13:01:53 +0900

    Add files via upload

--- PACMAN-canvas/src/actors.js
+++ PACMAN-canvas/src/actors.js
@@ -2,17 +2,18 @@ import {Actor}  from './actor.js'
 import {PacMan} from './actor.js'
 import {Ghosts} from './ghosts/_system.js'
 import {Ghost}  from './ghosts/ghost.js'
+import {player,onPlayerDotEaten} from './player/player.js'
 
 export {Actor,PacMan,Ghost,Ghosts}
-export {player,onPlayerDotEaten} from './player/player.js'
+export {player,onPlayerDotEaten}
 export const Actors = {
  	/** @param {PacMan} pacman */
-   update(pacman) {
+   update(pacman=player) {
         pacman.update()
         Ghosts.update()
     },
 	/** @param {PacMan} pacman */
-    draw(pacman) {
+    draw(pacman=player) {
         Ghosts.drawBehind()
         pacman.draw()
         Ghosts.drawFront()
--- PACMAN-canvas/src/scenes/main.js
+++ PACMAN-canvas/src/scenes/main.js
@@ -1,19 +1,18 @@
-import {Actors} from '../actors.js'
 import {Fruit}  from '../fruit.js'
 import {Maze}   from '../maze.js'
-import {player} from '../player/player.js'
 import {Score}  from '../score.js'
+import {Actors} from '../actors.js'
 
 export const MainScene = {
 	update() {
 		Fruit.update()
 		Maze.PowDots.update()
-		Actors.update(player)
+		Actors.update()
 	},
 	draw() {
 		Score.draw()
 		Maze.PowDots.draw()
 		Fruit.drawTarget()
-		Actors.draw(player)
+		Actors.draw()
 	},
 }
