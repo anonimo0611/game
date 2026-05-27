7467b45270e55eb53f4dc9e53ea76f5635bf90c2
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-05-27 16:00:33 +0900

    Add files via upload

--- PACMAN-canvas/src/actors.js
+++ PACMAN-canvas/src/actors.js
@@ -2,7 +2,7 @@ import {Actor}  from './actor.js'
 import {Ghosts} from './ghosts/_system.js'
 import {Ghost}  from './ghosts/ghost.js'
 import {PacMan,player,onPlayerDotEaten} from './pacman/pacman.js'
-export {Actor,PacMan,player,onPlayerDotEaten,Ghost,Ghosts}
+export {Actor,PacMan,Ghost,player,onPlayerDotEaten,Ghosts}
 export const Actors = {
     /** @param {PacMan} pacman */
     update(pacman=player) {
--- PACMAN-canvas/src/maze.js
+++ PACMAN-canvas/src/maze.js
@@ -59,7 +59,7 @@ class House {
 	isIn = tilePos=> HouseInner.contains(tilePos)
 
 	/** @param {Ghost} g */
-	arrived(g, spd=1) {
+	arrived(g, spd=g.speed) {
 		return g.state.isEscaping
 			&& g.tile.y == this.EntryTile.y
 			&& abs(BW/2 - g.center.x) <= spd
