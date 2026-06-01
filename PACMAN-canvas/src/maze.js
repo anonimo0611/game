3a52886c788beb0af39baf59904b4a351c5a3274
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-06-02 01:54:05 +0900

    Add files via upload

--- PACMAN-canvas/src/maze.js
+++ PACMAN-canvas/src/maze.js
@@ -41,15 +41,37 @@ D__________________________C\
 ////////////////////////////\
 ////////////////////////////`])
 
-const WallSet = /**@type {Set<TileIdx>}*/(new Set)
-const DotSet  = /**@type {Set<TileIdx>}*/(new Set)
-const PowMap  = /**@type {Map<TileIdx,Position>}*/(new Map)
-
 const DOT_RADIUS = T/9
 const POW_RADIUS = T/2
 const DotSymbols = new Set([...'.O'])
 const HouseInner = new Rect(14-4, 15-2,  8, 5).freeze()
 const HouseOuter = new Rect(14-5, 15-3, 10, 7).freeze()
+
+const WallSet = /**@type {Set<TileIdx>}*/(new Set)
+const DotSet  = /**@type {Set<TileIdx>}*/(new Set)
+const PowMap  = /**@type {Map<TileIdx,Position>}*/(new Map)
+
+;{//Map and dot initialization block
+	MapArr.forEach((s,i)=> {
+		!DotSymbols.has(s) && s.trim() && WallSet.add(i)
+	})
+	/** @param {JQTriggeredEvent} e */
+	function reset(e) {
+		e.target != powChk && Wall.draw()
+		MapArr.forEach((s,i)=> {
+			if (!DotSymbols.has(s)) return
+			const t = Vec2.new(i%COLS, i/COLS|0)
+			const m = Vec2.add(t, 0.5)
+			clearDot({tileIdx:i, tileMid:m})
+			DotSet.add(i)
+			powChk.checked == false || s == '.'
+				? drawDot(Bg, ...t.vals)
+				: PowMap.set(i, t)
+		})
+	}
+	State.on({_NewLevel: reset})
+	$(powChk).on({change:reset})
+}
 
 class House {
 	MID_Y = (this.EntryTile.y+3.5)*T
@@ -84,79 +106,59 @@ export class PowBlinker {
 }
 class PowDots extends PowBlinker {
 	draw() {
-		for (const {x,y} of PowMap.values())
+		PowMap.forEach(({x,y})=> {
 			if (!State.isInGame || Ticker.paused || this.show)
 				drawDot(Fg, x,y, true)
+		})
 	}
 }
 
-class MazeManager {
-	static {
-		for (const [i,s] of MapArr.entries())
-			!DotSymbols.has(s) && s.trim() && WallSet.add(i)
-		State.on({_NewLevel: this.reset})
-		$(powChk).on({change:this.reset})
-	}
-	static reset(/**@type {JQTriggeredEvent}*/e) {
-		e.target != powChk && Wall.draw()
-		for (const [i,s] of MapArr.entries())
-			DotSymbols.has(s) && MazeManager.setDot(i,s)
-	}
-	static setDot(/**@type {TileIdx}*/i, symbol='.') {
-		const t = Vec2.new(i%COLS, i/COLS|0)
-		const m = Vec2.add(t, 0.5)
-		clearDot({tileIdx:i, tileMid:m})
-		DotSet.add(i)
-		powChk.checked == false || symbol == '.'
-			? drawDot(Bg, ...t.vals)
-			: PowMap.set(i,t)
-	}
-	Map     = MapArr
-	MaxDot  = MapArr.filter(c=> DotSymbols.has(c)).length
-	House   = freeze(new House)
-	Tunnel  = freeze(new Tunnel)
-	PowDots = freeze(new PowDots)
-	hasDot  = (/**@type {TileIdx} */i)=> DotSet.has(i)
-	hasPow  = (/**@type {TileIdx} */i)=> PowMap.has(i)
-	hasWall = (/**@type {Position}*/p)=> WallSet.has(p.y*COLS+p.x)
+export const Maze = freeze({
+	Map:     MapArr,
+	MaxDot:  MapArr.filter(c=> DotSymbols.has(c)).length,
+	House:   freeze(new House),
+	Tunnel:  freeze(new Tunnel),
+	PowDots: freeze(new PowDots),
+	hasDot:  (/**@type {TileIdx} */i)=> DotSet.has(i),
+	hasPow:  (/**@type {TileIdx} */i)=> PowMap.has(i),
+	hasWall: (/**@type {Position}*/p)=> WallSet.has(p.y*COLS+p.x),
 
-	/** @readonly */
-	CLEAR_DOTS = 0 // for debug
-	get dotsLeft() {return DotSet.size}
+	CLEAR_DOTS: 0, // for debug
+	get dotsLeft() {return DotSet.size},
 
 	/**
 	 Tiles where the specified direction is disallowed for ghost navigation \
 	 Format ex: `${x}-${y}${Direction}`
 	 @type {ReadonlySet<string>}
 	*/
-	GhostNoEntryTiles = new Set(['12-11Up','12-23Up','15-11Up','15-23Up'])
+	GhostNoEntryTiles: new Set(['12-11Up','12-23Up','15-11Up','15-23Up']),
 
 	/**
 	 Whether tile `row` coord is the top/bottom of the maze excluding dead space
 	 @param {number} row
 	*/
-	isTopOrBottom = row=> (row == 1) || (row == ROWS-3)
+	isTopOrBottom: row=> (row == 1) || (row == ROWS-3),
 
 	/**
 	 If the target tile is on the upper side of the maze \
 	 and the ghost is around the house, guide them outside of the area
 	 @param {{baseTargetTile:Vec2,tile:Vec2}} ghost
 	*/
-	getGhostExitTile = ({baseTargetTile:b,tile:t})=>
+	getGhostExitTile: ({baseTargetTile:b,tile:t})=>
 		!Cfg.unrestricted && b.y < 10 && HouseOuter.contains(t)
-			? Vec2.new(t.x>COLS/2 && b.x>COLS/2 ? 21:6, 15) : b
+			? Vec2.new(t.x>COLS/2 && b.x>COLS/2 ? 21:6, 15) : b,
 
 	/**
 	 @param {{tileIdx:number,tileMid:Vec2}} tile
 	 @returns {number} Number of remaining dots
 	*/
-	clearDot({tileIdx:i,tileMid:{x,y}}) {
+	clearDot({tileIdx:i, tileMid:{x,y}}) {
 		const r = DOT_RADIUS+1
 		DotSet.delete(i)
 		PowMap.delete(i)
 		Bg.clearRect(x*T-r, y*T-r, r*2, r*2)
 		return DotSet.size
-	}
+	},
 
 	/**
 	 @param {EnhancedCtx2D} ctx
@@ -168,7 +170,6 @@ class MazeManager {
 		const [x,y] = [col,row].map(v=> (v+0.5)*T)
 		const r = (isPow? POW_RADIUS : DOT_RADIUS)
 		ctx.fillCircle(x,y, r, Color.Cookie)
-	}
-}
-export const Maze = freeze(new MazeManager)
+	},
+})
 export const {drawDot,clearDot}= Maze
