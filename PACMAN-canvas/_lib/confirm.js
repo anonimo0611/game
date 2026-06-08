dc8d98521d5cd8abe3134c0ddff1282f262c138b
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-06-08 13:26:54 +0900

    Add files via upload

--- PACMAN-canvas/_lib/confirm.js
+++ PACMAN-canvas/_lib/confirm.js
@@ -1,22 +1,25 @@
 import {Dir} from './direction.js'
 export const Confirm = new class ConfirmCore {
-	#opened = false
-	#cancel = /**@type {0|1}*/(0)
-	get opened()   {return this.#opened}
-	get #tempElm() {return /**@type {HTMLTemplateElement}*/(qSel('#confirm_t'))}
-	get #confirm() {return /**@type {HTMLDialogElement}  */(qSel('#confirm'))}
+	#opened    = false
+	#cancelIdx = /**@type {0|1}*/(0)
+	get opened() {return this.#opened}
 
 	/** @param {JQKeyboardEvent} e */
 	#onKeydown(e) {
 		const btns = $('#confirm button').get()
 		if (e.key == 'Escape') {
 			e.preventDefault()
-			return btns[Confirm.#cancel].click()
+			return btns[Confirm.#cancelIdx].click()
 		}
 		if (e.target instanceof HTMLButtonElement) {
 			const i = $(e.target).index()
 			Dir.from(e) == [R,L][i] && btns[1^i].focus()
 		}
+	}
+	#appendDialog() {
+		const temp = requireElem('confirm_t',HTMLTemplateElement)
+		document.body.append(temp.content.cloneNode(true))
+		return requireElem('confirm',HTMLDialogElement)
 	}
 
 	/**
@@ -28,24 +31,28 @@ export const Confirm = new class ConfirmCore {
 	 @param {0|1} [cancelIdx]  Button to assign when canceling; 0=left(default), 1=right
 	 @param {0|1} [autoFocus]  0=left, 1=right; The default is the same as `cancelIdx`
 	*/
-	open(content, cb1,cb2, btn1Txt='Cancel',btn2Txt='Ok', cancelIdx=0, autoFocus=cancelIdx) {
+	open(content,
+		cb1,cb2, btn1Txt='Cancel',btn2Txt='Ok',
+		cancelIdx=0, autoFocus=this.#cancelIdx=cancelIdx
+	) {
 		if (this.opened) return
 		this.#opened = true
-		this.#cancel = cancelIdx
-		document.body.append(this.#tempElm.content.cloneNode(true))
-		$(this.#confirm).find('.content').text(content)
-		$('#confirm').find('button').each((i,btn)=> {
+		const
+		$dialog = $(this.#appendDialog())
+		$dialog.fadeIn(300)[0].showModal()
+		$dialog.find('.content').text(content)
+		$dialog.find('button').each((i,btn)=> {
 			i == autoFocus && (btn.autofocus=true)
 			btn.classList.add(i == cancelIdx ? 'cancel':'ok')
 			btn.textContent = [btn1Txt,btn2Txt][i]
-			btn.onclick = ()=> {$win.off(NS);this.#remove([cb1,cb2][i])}
+			btn.onclick = ()=> this.#remove([cb1,cb2][i])
 		})
-		$(this.#confirm).fadeIn(300).get(0)?.showModal()
 		$win.onNS(NS,{keydown:this.#onKeydown})
 		$win.onNS(NS,{pointerdown:e=> e.preventDefault()})
 	}
 	/** @param {?()=> void} cb */
 	#remove(cb) {
+		$win.off(NS)
 		$('#confirm').fadeOut(300, function() {
 			this.remove(), cb?.()
 			Confirm.#opened = false
