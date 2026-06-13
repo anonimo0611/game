a6b6e8b46ed86d30d62ae19d13465c749201574b
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-06-13 11:25:57 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/snag.js
+++ PACMAN-canvas/src/sprites/snag.js
@@ -21,7 +21,7 @@ export class SnagSprite {
 		ctx.fillRect(0,-sh, sw,sh, 'white') // Stake
 		if (isRipped) { // Scrap of cloth
 			const s = sw*0.8
-			ctx.translate(sw, 0)
+		ctx.translate(sw, 0)
 			ctx.fillPolygon(AKA_COLOR, [0,-s],[0,-sh],[-T,0],[-s,0],[-s,-s])
 		}
 		ctx.restore()
@@ -34,18 +34,16 @@ export class SnagSprite {
 	*/
 	drawSnaggedClothing(animIdx, ratio, {x=0,y=0,scale=1}={}) {
 		const {ctx}= this
-		const v1 = mathLerp(-2,  5, ratio)
-		const v2 = mathLerp(+4, 22, ratio)
-		const v3 = mathLerp(+4, 50, ratio)
+		const v1 = mathLerp(+4, 22, ratio)
+		const v2 = mathLerp(+4, 50, ratio)
 		const ls = (animIdx? -25 : -36) // Left side
 		ctx.save()
 		ctx.translate(x, y)
 		ctx.scale(T*2*scale/LOGIC_SIZE)
 		ctx.beginPath()
-		ctx.moveTo(-8, -10)
-		ctx.quadraticCurveTo(-8, -4, v1, 3)
-		ctx.quadraticCurveTo(v2, 9, v3, 9)
-		ctx.addLinePath([v3,42],[ls,42],[ls,20],[-8,20])
+		ctx.moveTo(-11, -20) //-11 -20
+		ctx.quadraticCurveTo(v1, 9, v2, 9)
+		ctx.addLinePath([v2,42],[ls,42],[ls,20],[-8,20])
 		ctx.fillStyle = AKA_COLOR
 		ctx.fill()
 		ctx.restore()
