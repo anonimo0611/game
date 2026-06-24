4d6953aea1dfd2780b6e1e392b913deb1c8159f8
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-06-25 04:10:57 +0900

    Add files via upload

--- PACMAN-canvas/_lib/confirm.js
+++ PACMAN-canvas/_lib/confirm.js
@@ -8,7 +8,7 @@ export const Confirm = new class ConfirmCore {
 		const btns = $('#confirm button').get()
 		if (e.key == 'Escape') {
 			e.preventDefault()
-			return $(btns).filter('.cancel')[0]?.click()
+			return $(btns).filter('.cancel')[0].click()
 		}
 		if (e.target instanceof HTMLButtonElement) {
 			const i = $(e.target).index()
@@ -18,42 +18,38 @@ export const Confirm = new class ConfirmCore {
 	#appendDialog() {
 		const temp = reqElem('#confirm_t',HTMLTemplateElement)
 		document.body.append(temp.content.cloneNode(true))
-		return reqElem('#confirm',HTMLDialogElement)
+		return $(reqElem('#confirm',HTMLDialogElement))
 	}
 
 	/**
-	 @param {string} content   Dialog description
-	 @param {?()=> void} cb1   Functions to assign to the left button
-	 @param {?()=> void} cb2   Functions to assign to the right button
-	 @param {string} [btn1Txt] Text of the left  button
-	 @param {string} [btn2Txt] Text of the right button
-	 @param {0|1} [cancelIdx]  Button to assign when canceling; 0=left(default), 1=right
-	 @param {0|1} [autoFocus]  0=left, 1=right; The default is the same as `cancelIdx`
+	 @param {string} content  Dialog description
+	 @param {[?()=> void,?()=> void]} callbacks Callbacks assigned to the left and right buttons
+	 @param {[string,string]} btnTexts Left and right button text
+	 @param {0|1} [cancelIdx] Button to assign when canceling; 0=left(default), 1=right
+	 @param {0|1} [autoFocus] 0=left, 1=right; The default is the same as `cancelIdx`
 	*/
-	open(content, cb1,cb2, btn1Txt='Cancel', btn2Txt='Ok', cancelIdx=0, autoFocus=cancelIdx) {
+	open(content, [cb0,cb1], [btn0Txt,btn1Txt]=['Cancel','Ok'], cancelIdx=0, autoFocus=cancelIdx) {
 		if (this.opened) return
-		this.#opened  = true
+		this.#opened = true
 		const
-		$dialog = $(this.#appendDialog())
+		$dialog = this.#appendDialog()
 		$dialog.find('.content').text(content)
 		$dialog.find('button').each((i,btn)=> {
 			if (i == autoFocus) btn.autofocus = true
 			btn.classList.add(i == cancelIdx? 'cancel':'ok')
-			btn.textContent = [btn1Txt,btn2Txt][i]
+			btn.textContent = [btn0Txt,btn1Txt][i]
 			btn.onclick = ()=> {
-				$win.off(NS)
 				$dialog.fadeOut(300, function() {
 					this.close()
 					this.remove()
-					;[cb1,cb2][i]?.()
+					;[cb0,cb1][i]?.()
 					Confirm.#opened = false
 				})
 			}
 		})
-		$win.onNS(NS, {
+		$dialog.on({
 			keydown:this.#onKeydown,
 			pointerdown:e=> e.preventDefault()
-		})
-		$dialog.fadeIn(300)[0].showModal()
+		}).fadeIn(300)[0].showModal()
 	}
-}, NS = '.CONFIRM'
+}
