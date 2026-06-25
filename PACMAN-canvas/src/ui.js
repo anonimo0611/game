c1f382555b44a79e7e2795d00b60d1873f1cda74
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-06-25 19:57:35 +0900

    Add files via upload

--- PACMAN-canvas/src/ui.js
+++ PACMAN-canvas/src/ui.js
@@ -2,22 +2,20 @@ export const Form   = document.forms[0]
 export const powChk = reqInput('#powEnabled')
 export const lives  = reqInput('#initialLives')
 
-{// Set grid lines
-	Grid.beginPath()
-	for(let x=1; x<COLS; x++) Grid.setLinePath([T*x, 0],[T*x, BH])
-	for(let y=0; y<ROWS; y++) Grid.setLinePath([0, T*y],[BW, T*y])
-	Grid.strokeStyle = Color.GridLine
-	Grid.stroke()
-}
+//---- Grid lines ----
+
+Grid.beginPath()
+for(let x=1; x<COLS; x++) Grid.setLinePath([T*x, 0],[T*x, BH])
+for(let y=0; y<ROWS; y++) Grid.setLinePath([0, T*y],[BW, T*y])
+Grid.strokeStyle = Color.GridLine
+Grid.stroke()
 
 //---- Fit to viewport ----
 
 $win.on('resize', ()=> {
-	const scale = min(
-		innerWidth /Form.offsetWidth*.98,
-		innerHeight/Form.offsetHeight
-	)
-	Form.style.scale = min(1, scale).toFixed(2)
+	const {offsetWidth:w,offsetHeight:h}= Form
+	const s = min(innerWidth/w*.98, innerHeight/h)
+	Form.style.scale = min(1,s).toFixed(2)
 })
 .trigger('resize')
 
@@ -53,8 +51,7 @@ import * as Fruits from './sprites/fruits.js'
 //---- Pop over ----
 
 $('body').on('keydown pointerdown', e=> {
-	if (e.key == 'Escape'
-	|| !e.target.closest('.popover'))
+	if (e.key == 'Escape' || !e.target.closest('.popover'))
 		$('.popover').removeClass('opened')
 })
 $('button.popover').on('keydown pointerdown', e=> {
