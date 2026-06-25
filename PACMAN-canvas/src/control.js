80bad046335c450790dec477e2802f9e71eeab7b
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-06-26 06:12:24 +0900

    Add files via upload

--- PACMAN-canvas/src/control.js
+++ PACMAN-canvas/src/control.js
@@ -5,11 +5,13 @@ import {State}    from './state.js'
 import {drawText} from './message.js'
 import {Form,Menu,btns} from './ui.js'
 
-const SETTINGS_KEY = 'anopacman'
 const {InfoTexts:palette}= Color
-
+const SETTINGS_KEY  = 'anopacman'
+const LOW_SPD_LIMIT = 0.7
 const Ctl = {
 	speed:         1,
+	isStAbove1:    false,
+	isLowSpeed:    false,
 	currentOnly:   false,
 	alwaysChase:   false,
 	unrestricted:  false,
@@ -18,6 +20,10 @@ const Ctl = {
 	showPaths:     false,
 	showGridLines: false,
 }
+const PracticeIds = /**@type {const}*/([
+	'isStAbove1','isLowSpeed','currentOnly',
+	'invincible','showPaths','showTargets'
+])
 
 export const Env = new class Environment {
 	static {$(this.setup)}
@@ -31,7 +37,7 @@ export const Env = new class Environment {
 	get extendScore()  {return +Menu.Extend.value}
 	get showTracking() {return Ctl.showTargets || Ctl.showPaths}
 	get semiTransPac() {return Ctl.invincible  || Ctl.showGridLines}
-	get usingCheats()  {return Ctl.invincible  || Ctl.speed<.7 || Env.showTracking}
+	get usingCheats()  {return Ctl.invincible  || Ctl.isLowSpeed || Env.showTracking}
 	get isCaptured()   {return Env.#anyFocused || Confirm.opened}
 	get isPractice()   {return Env.usingCheats ||!Env.isArcadeMode}
 	get isArcadeMode() {return Ctl.currentOnly == false && !Menu.Level.index}
@@ -77,11 +83,12 @@ export const Env = new class Environment {
 		Grid.canvas.style.opacity = String(+Ctl.showGridLines)
 		const spd = 'x'+Ctl.speed.toFixed(1), lh = 0.9
 		const opt = {ctx:HUD, size:T*0.68, scaleX:0.7, style:'bold'}
+		Env.#setHelpPanelColors()
 		HUD.save()
 		HUD.translate(T*0.1, T*17.25)
 		HUD.clearRect(0, 0, BW, T*3)
 		if (spd != 'x1.0' || Ctl.invincible || Ctl.showTargets) {
-			drawText(0, lh*0, palette[+(spd != 'x1.0') ], 'Speed'+spd, opt)
+			drawText(0, lh*0, palette[+(spd != 'x1.0')], 'Speed'+spd, opt)
 			drawText(0, lh*1, palette[+Ctl.invincible ], 'Invincible',opt)
 			drawText(0, lh*2, palette[+Ctl.showTargets], 'Show Tgts', opt)
 		}
@@ -91,6 +98,12 @@ export const Env = new class Environment {
 			drawText(0, lh, palette[+Ctl.unrestricted],'Ghosts Un-\nrestricted', opt)
 		}
 		HUD.restore()
+	}
+	#setHelpPanelColors() {
+		Ctl.isStAbove1 = (Menu.Level.index > 0)
+		Ctl.isLowSpeed = (Ctl.speed < LOW_SPD_LIMIT)
+		for (const id of PracticeIds)
+			$(`#_${id}`).css('color', palette[+Ctl[id]])
 	}
 	#reset() {
 		Form.reset()
@@ -129,7 +142,7 @@ export const Env = new class Environment {
 	}
 	#setupCtrls() {
 		Env.#observeFocusChange()
-		getVals(Menu).forEach(m=> m.onChange(Env.#save))
+		getVals(Menu).forEach(m=> m.onChange(Env.#output))
 		$('input')   .on({input:Env.#output})
 		$(btns.reset).on({click:Env.#reset})
 		$(btns.start).on({click:State.setNewGame})
