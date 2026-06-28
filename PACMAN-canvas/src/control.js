14cda343dadaed429d0072ed43376af35f97901c
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-06-28 14:22:55 +0900

    Add files via upload

--- PACMAN-canvas/src/control.js
+++ PACMAN-canvas/src/control.js
@@ -8,7 +8,9 @@ import {Form,Menu,btns} from './ui.js'
 const {InfoTexts:palette}= Color
 const SETTINGS_KEY = 'anopacman'
 const LOW_SPEED_THRESHOLD = 0.7
-const Ctl = {
+
+export const Cfg = /**@type {const}*/
+({
 	speed:         1,
 	isStAbove1:    false,
 	isLowSpeed:    false,
@@ -19,7 +21,7 @@ const Ctl = {
 	showTargets:   false,
 	showPaths:     false,
 	showGridLines: false,
-}
+})
 
 export const Env = new class Environment {
 	static {$(this.setup)}
@@ -30,12 +32,12 @@ export const Env = new class Environment {
 	}
 	#anyFocused = false
 	get extendScore()  {return +Menu.Extend.value}
-	get showTracking() {return Ctl.showTargets || Ctl.showPaths}
-	get semiTransPac() {return Ctl.invincible  || Ctl.showGridLines}
-	get usingCheats()  {return Ctl.invincible  || Ctl.isLowSpeed || Env.showTracking}
+	get showTracking() {return Cfg.showTargets || Cfg.showPaths}
+	get semiTransPac() {return Cfg.invincible  || Cfg.showGridLines}
+	get usingCheats()  {return Cfg.invincible  || Cfg.isLowSpeed || Env.showTracking}
 	get isCaptured()   {return Env.#anyFocused || Confirm.opened}
 	get isPractice()   {return Env.usingCheats ||!Env.isArcadeMode}
-	get isArcadeMode() {return Ctl.currentOnly == false && !Menu.Level.index}
+	get isArcadeMode() {return Cfg.currentOnly == false && !Menu.Level.index}
 
 	/** @readonly */
 	window = function() {
@@ -52,7 +54,7 @@ export const Env = new class Environment {
 		Sound.pause( Ticker.pause(force) )
 	}
 	#save() {
-		const data = /**@type {any}*/(Ctl)
+		const data = /**@type {any}*/(Cfg)
 		getKeys(Menu).forEach(id=> data[id] = Menu[id].index)
 		document.querySelectorAll('input').forEach(input=> {
 			switch(input.type) {
@@ -60,6 +62,8 @@ export const Env = new class Environment {
 			case 'checkbox':data[input.id] = input.checked;break
 			}
 		})
+		data.isStAbove1 = (Menu.Level.index > 0)
+		data.isLowSpeed = (Cfg.speed < LOW_SPEED_THRESHOLD)
 		localStorage[SETTINGS_KEY] = JSON.stringify(data)
 	}
 	#restore() {
@@ -78,28 +82,22 @@ export const Env = new class Environment {
 		Env.#syncHelpPanel()
 		Env.#toggleGridLines()
 		const
-		spd = 'x'+Ctl.speed.toFixed(1), lh = 0.9,
+		spd = 'x'+Cfg.speed.toFixed(1), lh = 0.9,
 		opt = {ctx:HUD, size:T*0.68, scaleX:0.7, style:'bold'}
 		HUD.save()
 		HUD.translate(T*0.1, T*17.25)
 		HUD.clearRect(0, 0, BW, T*3)
-		if (spd != 'x1.0' || Ctl.invincible || Ctl.showTargets) {
+		if (spd != 'x1.0' || Cfg.invincible || Cfg.showTargets) {
 			drawText(0, lh*0, palette[+(spd != 'x1.0')], 'Speed'+spd, opt)
-			drawText(0, lh*1, palette[+Ctl.invincible ], 'Invincible',opt)
-			drawText(0, lh*2, palette[+Ctl.showTargets], 'Show Tgts', opt)
+			drawText(0, lh*1, palette[+Cfg.invincible ], 'Invincible',opt)
+			drawText(0, lh*2, palette[+Cfg.showTargets], 'Show Tgts', opt)
 		}
-		if (Ctl.showPaths || Ctl.unrestricted) {
+		if (Cfg.showPaths || Cfg.unrestricted) {
 			HUD.translate(T*(COLS-5), 0)
-			drawText(0,  0, palette[+Ctl.showPaths],   'Show Paths', opt)
-			drawText(0, lh, palette[+Ctl.unrestricted],'Ghosts Un-\nrestricted', opt)
+			drawText(0,  0, palette[+Cfg.showPaths],   'Show Paths', opt)
+			drawText(0, lh, palette[+Cfg.unrestricted],'Ghosts Un-\nrestricted', opt)
 		}
 		HUD.restore()
-	}
-	#syncHelpPanel() {
-		Ctl.isStAbove1 = (Menu.Level.index > 0)
-		Ctl.isLowSpeed = (Ctl.speed < LOW_SPEED_THRESHOLD)
-		for (const id of getKeys(Ctl))
-			$(`#_${id}`).css('color', palette[+Ctl[id]])
 	}
 	#reset() {
 		Form.reset()
@@ -131,7 +129,11 @@ export const Env = new class Environment {
 		}
 	}
 	#toggleGridLines() {
-		Grid.canvas.style.opacity = String(+Ctl.showGridLines)
+		Grid.canvas.style.opacity = String(+Cfg.showGridLines)
+	}
+	#syncHelpPanel() {
+		for (const id of getKeys(Cfg))
+			$(`#_${id}`).css('color', palette[+Cfg[id]])
 	}
 	#observeFocusChange() {
 		$(document.body).on('focusin focusout', e=> {
@@ -147,4 +149,4 @@ export const Env = new class Environment {
 		$(btns.start).on({click:State.setNewGame})
 		$root.addClass('controller-settled')
 	}
-}, Cfg = /**@type {Readonly<typeof Ctl>}*/(Ctl)
+}
