77ab863dfc3fdd9c3b8ac1d6438065e66e3e19d9
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-07-21 07:28:26 +0900

    Add files via upload

--- PACMAN-canvas/src/sprites/fruits.js
+++ PACMAN-canvas/src/sprites/fruits.js
@@ -150,28 +150,27 @@ function melon(ctx) {
 },
 function gala(ctx) {
 	const yellow = '#F8FF00'
-	ctx.translate(0, .2)
 	for (const scaleX of [1,-1]) {
 		// yellow body
 		ctx.save()
 		ctx.scale(scaleX, 1)
-		ctx.fillPolygon(yellow,[0,-3.1],[4.9,-1.6],[4.6,0],[1.6,2],[1.6,1],[0,1])
+		ctx.fillPolygon(yellow,[0,-3],[4.9,-1.5],[4.6,0.2],[1.6,2.3],[1.6,1.6],[0,1.6])
 		// blue wings
-		ctx.fillPolygon('#0AF',[4.6,-4.3],[6,-4.3],[6,.7],[1.6,4],[1.6,2],[4.6,0])
+		ctx.fillPolygon('#0AF',[4.6,-4.5],[6,-4.6],[6,0.9],[1.6,4.2],[1.6,2.2],[4.6,0.2])
 		ctx.restore()
 	}
 	// yellow tail
 	ctx.lineWidth   = 1.3
 	ctx.strokeStyle = yellow
-	ctx.strokeLine(0, 0, 0, 6)
+	ctx.strokeLine(0, 0.1, 0, 6.4)
 
 	// red arrow head
 	ctx.beginPath()
 	for (const scaleX of [1,-1]) {
 		ctx.scale(scaleX, 1)
-		ctx.setLinePath([0,-5.5],[4.3,-1.7],[1.3,-2.4])
-		ctx.quadraticCurveTo(1, -2.4, 1, -2.4)
-		ctx.quadraticCurveTo(0.4, 0.1, 0, 0.1)
+		ctx.setLinePath([0,-5.4],[4.6,-1.4],[1.3,-1.9])
+		ctx.quadraticCurveTo(1.2, -1.9, 1.2, -1.9)
+		ctx.quadraticCurveTo(0.4, 0.6, 0, 0.6)
 	}
 	ctx.fillStyle = '#FF3401'
 	ctx.fill()
