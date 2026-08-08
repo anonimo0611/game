ee2ea8a85362f5c6d9149a73050f1669e9c4e7a1
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-08-08 18:28:26 +0900

    Add files via upload

--- PACMAN-canvas/_lib/_types.d.ts
+++ PACMAN-canvas/_lib/_types.d.ts
@@ -4,8 +4,8 @@ type GhostHitRadii = readonly [normal: number, frightened: number]
 
 //---- Dictionary ----
 
-type Scene  = {update():void, draw?():void}
-type SceneDict<T extends string> = {[K in T]?:Scene}
+type Scene  = {update():void, draw():void}
+type SceneDict<T extends string> = {readonly [K in T]?:Scene}
 type NumericDict<T> = {readonly [K:number]:T}
 
 //---- Timer ----
