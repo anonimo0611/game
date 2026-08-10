50125ef2a2396e5a7ba50a5827072f67d4f042a9
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-08-11 00:19:00 +0900

    Add files via upload

--- PACMAN-canvas/src/lives.js
+++ PACMAN-canvas/src/lives.js
@@ -5,12 +5,18 @@ import  Sprite from './sprites/pacman.js'
 export const Lives = function() {
 	let   left   = 0
 	const SIZE   = T*2
-	const sprite = new Sprite(HUD, T*.8, 0.5)
+	const sprite = new Sprite(HUD, T*0.8, 0.5)
 	State.on({
 		Title:   ()=> reset(),
 		NewGame: ()=> add(+1),
 		Ready:   ()=> add(State.wasNewLevel? 0:-1),
 	})
+	function add(n=0) {
+		draw(left += n)
+	}
+	function reset() {
+		draw(left = lives.valueAsNumber-1)
+	}
 	function draw(left=3) {
 		HUD.save()
 		HUD.translate(SIZE+T, BH-T)
@@ -19,8 +25,6 @@ export const Lives = function() {
 			sprite.draw({center:{x:SIZE*i}})
 		HUD.restore()
 	}
-	function add(n=0) {draw(left += n)}
-	function reset()  {draw(left = lives.valueAsNumber-1)}
 	$(lives).on({input:reset})
 	return {
 		extend()   {add(+1)},
