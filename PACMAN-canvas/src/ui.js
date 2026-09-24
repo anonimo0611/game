5bd4f2292e8d0f7fba4b4c244045afb0939f1cd0
anonimo0611 <111575913+anonimo0611@users.noreply.github.com>
2026-09-24 22:30:06 +0900

    Add files via upload

--- PACMAN-canvas/src/ui.js
+++ PACMAN-canvas/src/ui.js
@@ -5,48 +5,12 @@ export const Form   = document.forms[0]
 export const powChk = reqInput('#powEnabled')
 export const lives  = reqInput('#initialLives')
 
-//---- Fit to viewport ----
-
-$win.on('resize', ()=> {
-	const {offsetWidth:w,offsetHeight:h}= Form
-	const s = min(innerWidth/w*.98, innerHeight/h)
-	Form.style.scale = min(1,s).toFixed(2)
-})
-.trigger('resize')
-
-//---- Pop over ----
-
-$('body').on('keydown pointerdown', e=> {
-	if (e.key == 'Escape' || !e.target.closest('.popover'))
-		$('.popover').removeClass('opened')
-})
-$('button.popover').on('keydown pointerdown', e=> {
-	if (e.key && !isActionKey(e)) return
-	const btn = reqButton(e.currentTarget)
-	const opn = $(btn).hasClass('opened')
-	$('.popover.opened') .removeClass('opened')
-	$(btn).add(btn.value).toggleClass('opened',!opn)
-})
-.on('focus', function() {
-	const p = $('.popover.opened').get(0)
-	p && p != this && $(this).trigger('pointerdown')
-})
-
-//---- Grid lines ----
-
-Grid.beginPath()
-for(let x=1; x<COLS; x++) Grid.setLinePath([T*x, 0],[T*x, BH])
-for(let y=0; y<ROWS; y++) Grid.setLinePath([0, T*y],[BW, T*y])
-Grid.strokeStyle = Color.GridLine
-Grid.stroke()
-
 //---- Buttons ----
 
 export const Btns = function() {
-	const ids = /**@type {const}*/(['clear','reset','start'])
-	return /**@type {{[K in ids[number]]:HTMLButtonElement}}*/(
-		toObj(ids.map(id=> [id,reqButton(`#${id}Btn`)]))
-	)
+	const ids = /**@type {const}*/(['clearBtn','resetBtn','startBtn'])
+	const ret = toObj(ids.map(id=> [id,reqButton(`#${id}`)]))
+	return /**@type {{[K in ids[number]]:HTMLButtonElement}}*/(ret)
 }()
 
 //---- Custom Menus ----
@@ -63,3 +27,39 @@ export const Menu = freeze({
 		Fruits.draw(ctx, i, size, i*size + size/2)
 	$(menu).css('--url',`url("${ctx.canvas.toDataURL()}")`)
 }
+
+//---- Grid lines ----
+
+Grid.beginPath()
+for(let x=1; x<COLS; x++) Grid.setLinePath([T*x, 0],[T*x, BH])
+for(let y=0; y<ROWS; y++) Grid.setLinePath([0, T*y],[BW, T*y])
+Grid.strokeStyle = Color.GridLine
+Grid.stroke()
+
+//---- Fit to viewport ----
+
+$win.on('resize', ()=> {
+	const {offsetWidth:w,offsetHeight:h}= Form
+	const s = min(innerWidth/w*.98, innerHeight/h)
+	Form.style.scale = min(1,s).toFixed(2)
+})
+.trigger('resize')
+
+//---- Pop over ----
+
+const closeAllPanel = ()=>
+	$('.popover').removeClass('opened')
+
+$('body').on('keydown pointerdown', e=> {
+	if (e.key == 'Escape'
+	|| !e.target.closest('.popover'))
+		closeAllPanel()
+})
+$('button.popover').on('keydown pointerdown', e=> {
+	if (e.key && !isActionKey(e)) return
+	const btn = reqButton(e.currentTarget)
+	const opn = $(btn).hasClass('opened')
+	$('.popover.opened') .removeClass('opened')
+	$(btn).add(btn.value).toggleClass('opened',!opn)
+})
+.on({blur:closeAllPanel})
